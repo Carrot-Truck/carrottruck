733cbece24bdf18390f1d206f7f7870b996c7c8d
gunhoo <gunhoo2016@gmail.com>
2023-10-24 14:14:30 +0900

    feat: Review-Comment Entity 작성
    With S09P31C211-102

---
+++ Backend/src/main/java/com/boyworld/carrot/domain/review/Comment.java
@@ -0,0 +1,67 @@
+package com.boyworld.carrot.domain.review;
+
+import com.boyworld.carrot.domain.TimeBaseEntity;
+import com.boyworld.carrot.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * Review-Comment Entity
+ *
+ * @author Gunhoo Park
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class Comment extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinTable(name = "review_id")
+    @Column(nullable = false)
+    private Review review;
+
+    @ManyToOne
+    @JoinTable(name = "vendor_id")
+    @Column(nullable = false)
+    private Member member;
+
+    @Column(columnDefinition = "text", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(nullable = false)
+    private LocalDateTime modifiedDate;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Builder
+    private Comment(Review review, Member member, String content, LocalDateTime createdDate,
+        LocalDateTime modifiedDate, Boolean active) {
+        this.review = review;
+        this.member = member;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+        this.active = active;
+    }
+}

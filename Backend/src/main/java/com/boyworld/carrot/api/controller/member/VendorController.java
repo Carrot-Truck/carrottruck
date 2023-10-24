54b6f696e494097fcc0718665510e1b34dd38a87
최영환 <longbright0804@gmail.com>
2023-10-24 14:09:08 +0900

    docs: 사업자 회원가입 API Docs 작성 - VendorControllerDocsTest - index.adoc - vendor.adoc - index.html
    Closes #S09P31C211-147

--- Backend/src/main/java/com/boyworld/carrot/api/controller/member/VendorController.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/member/VendorController.java
@@ -7,10 +7,8 @@ import com.boyworld.carrot.api.service.member.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 사업자 관련 API 컨트롤러
@@ -32,6 +30,7 @@ public class VendorController {
      * @return 가입된 회원 정보
      */
     @PostMapping("/vendor/join")
+    @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<JoinMemberResponse> join(@Valid @RequestBody JoinRequest request) {
         log.debug("ClientController#join called !!!");
         log.debug("JoinRequest={}", request);

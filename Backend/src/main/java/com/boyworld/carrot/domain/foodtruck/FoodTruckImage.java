4aee4f33833975bfb40fe2bb96e32f3ffc78717e
최영환 <longbright0804@gmail.com>
2023-10-24 11:51:41 +0900

    feat: 푸드트럭 이미지 Entity 구현 - FoodTruckImage - UploadFile - FileStore
    Closes #S09P31C211-148

---
+++ Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/FoodTruckImage.java
@@ -0,0 +1,42 @@
+package com.boyworld.carrot.domain.foodtruck;
+
+
+import com.boyworld.carrot.domain.TimeBaseEntity;
+import com.boyworld.carrot.file.UploadFile;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 푸드트럭 이미지
+ *
+ * @author 최영환
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class FoodTruckImage extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "food_truck_image_id")
+    private Long id;
+
+    @Embedded
+    UploadFile uploadFile;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "truck_id")
+    private FoodTruck foodTruck;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Builder
+    private FoodTruckImage(UploadFile uploadFile, FoodTruck foodTruck) {
+        this.uploadFile = uploadFile;
+        this.foodTruck = foodTruck;
+    }
+}

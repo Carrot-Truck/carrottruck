09a4932574d6b49f26405494779e9ee1d4ede7b2
최영환 <longbright0804@gmail.com>
2023-10-24 10:29:15 +0900

    feat: 회원가입 API Controller 스켈레톤 작성 - ClientController - JoinRequest - JoinMemberResponse
    Closes #S09P31C211-147

---
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/member/request/JoinRequest.java
@@ -0,0 +1,46 @@
+package com.boyworld.carrot.api.controller.member.request;
+
+import com.boyworld.carrot.api.service.member.dto.JoinMemberDto;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class JoinRequest {
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String nickname;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @Builder
+    public JoinRequest(String email, String nickname, String password, String name, String phoneNumber) {
+        this.email = email;
+        this.nickname = nickname;
+        this.password = password;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public JoinMemberDto toJoinMemberDto() {
+        return JoinMemberDto.builder()
+                .email(this.email)
+                .nickname(this.nickname)
+                .password(this.password)
+                .name(this.name)
+                .phoneNumber(this.phoneNumber)
+                .build();
+    }
+}
---
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/member/response/JoinMemberResponse.java
@@ -0,0 +1,7 @@
+package com.boyworld.carrot.api.controller.member.response;
+
+import lombok.Data;
+
+@Data
+public class JoinMemberResponse {
+}

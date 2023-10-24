ac61d82d1a738d72653387ca2c6c8afbe8b35166
gunhoo <gunhoo2016@gmail.com>
2023-10-24 16:51:08 +0900

    fix: Review Status enum 변경

--- Backend/src/main/java/com/boyworld/carrot/domain/review/Status.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/review/Status.java
@@ -5,14 +5,14 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * Review Report status enum
+ *
  * @author Gunhoo Park
  */
 @Getter
 @RequiredArgsConstructor
 public enum Status {
-    WAITING("대기중"),
     PROCESSING("처리중"),
-    DONE("처리완료");
+    COMPLETE("처리완료");
 
     private final String text;
 }

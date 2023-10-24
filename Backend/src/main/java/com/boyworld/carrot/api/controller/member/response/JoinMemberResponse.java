656ccf152e281f01ecec05888e8d091cb25718e6
최영환 <longbright0804@gmail.com>
2023-10-24 10:54:11 +0900

    docs: 회원가입 API Docs 작성 - ClientControllerDocsTest - JoinMemberResponse - index.adoc - overview.adoc - client.adoc
    Closes #S09P31C211-147

--- Backend/src/main/java/com/boyworld/carrot/api/controller/member/response/JoinMemberResponse.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/member/response/JoinMemberResponse.java
@@ -1,7 +1,34 @@
 package com.boyworld.carrot.api.controller.member.response;
 
+import com.boyworld.carrot.domain.member.Member;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class JoinMemberResponse {
+
+    private String email;
+    private String nickname;
+    private String name;
+    private String phoneNumber;
+    private String role;
+
+    @Builder
+    public JoinMemberResponse(String email, String nickname, String name, String phoneNumber, String role) {
+        this.email = email;
+        this.nickname = nickname;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public static JoinMemberResponse of(Member member) {
+        return JoinMemberResponse.builder()
+                .email(member.getEmail())
+                .name(member.getName())
+                .nickname(member.getNickname())
+                .phoneNumber(member.getPhoneNumber())
+                .role(member.getRole().toString())
+                .build();
+    }
 }

c22cf4f8cb80e26d68654835045828f2c92c3e3c
박은규 <parkeg1223@gmail.com>
2023-10-24 12:02:55 +0900

    feat: 주문 Entity 구현

---
+++ Backend/src/main/java/com/boyworld/carrot/domain/order/Order.java
@@ -0,0 +1,53 @@
+package com.boyworld.carrot.domain.order;
+
+import com.boyworld.carrot.domain.TimeBaseEntity;
+import com.boyworld.carrot.domain.member.Member;
+import com.boyworld.carrot.domain.sale.Sale;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 주문 엔티티
+ *
+ * @author 박은규
+ */
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "orders_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "sales_id", nullable = false)
+    private Sale sales;
+
+    @Column(length = 100, nullable = false)
+    private String status;
+
+    @Column
+    private LocalDateTime expectTime;
+
+    @Column(nullable = false)
+    private Integer totalPrice;
+
+    @Column(nullable = false)
+    private Boolean active;
+}

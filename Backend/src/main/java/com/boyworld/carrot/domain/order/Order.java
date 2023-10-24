ccacfab152ebce920e5cc5f57c49fca268cd4039
박은규 <parkeg1223@gmail.com>
2023-10-24 14:52:06 +0900

    fix: table 어노테이션 추가

--- Backend/src/main/java/com/boyworld/carrot/domain/order/Order.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/order/Order.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -24,6 +25,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
 public class Order extends TimeBaseEntity {
 
     @Id

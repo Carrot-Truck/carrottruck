ef9f8a24a1959d26872358c7b71a3c03f09299ce
longBright <longbright0804@gmail.com>
2023-10-25 20:23:17 +0900

    docs: 푸드트럭 등록 API Docs 작성 - FoodTruckControllerDocsTest#createFoodTruck - food-truck.adoc - index.adoc - index.html
    Closes #S09P31C211-150

---
+++ Backend/src/test/java/com/boyworld/carrot/docs/foodtruck/FoodTruckControllerDocsTest.java
@@ -0,0 +1,138 @@
+package com.boyworld.carrot.docs.foodtruck;
+
+import com.boyworld.carrot.api.controller.foodtruck.FoodTruckController;
+import com.boyworld.carrot.api.controller.foodtruck.request.CreateFoodTruckRequest;
+import com.boyworld.carrot.api.controller.foodtruck.response.FoodTruckResponse;
+import com.boyworld.carrot.api.service.foodtruck.FoodTruckService;
+import com.boyworld.carrot.api.service.foodtruck.dto.CreateFoodTruckDto;
+import com.boyworld.carrot.docs.RestDocsSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.web.multipart.MultipartFile;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.multipart;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FoodTruckControllerDocsTest.class)
+public class FoodTruckControllerDocsTest extends RestDocsSupport {
+
+    private final FoodTruckService foodTruckService = mock(FoodTruckService.class);
+
+    @Override
+    protected Object initController() {
+        return new FoodTruckController(foodTruckService);
+    }
+
+    @DisplayName("푸드트럭 등록 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
+    void createFoodTruck() throws Exception {
+        CreateFoodTruckRequest request = CreateFoodTruckRequest.builder()
+                .categoryId(1L)
+                .foodTruckName("동현 된장삼겹")
+                .phoneNumber("010-1234-5678")
+                .content("된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭")
+                .originInfo("돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)")
+                .prepareTime(40)
+                .waitLimits(10)
+                .build();
+
+        MockMultipartFile file = new MockMultipartFile("file", "image.jpg", MediaType.IMAGE_JPEG_VALUE, "image data".getBytes());
+
+        FoodTruckResponse response = FoodTruckResponse.builder()
+                .foodTruckId(1L)
+                .categoryId(1L)
+                .foodTruckName("동현 된장삼겹")
+                .categoryName("고기/구이")
+                .phoneNumber("010-1234-5678")
+                .content("된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭")
+                .originInfo("돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)")
+                .prepareTime(40)
+                .waitLimits(10)
+                .build();
+
+        String jsonRequest = objectMapper.writeValueAsString(request);
+        MockMultipartFile jsonRequestPart = new MockMultipartFile("request", "request.json", APPLICATION_JSON_VALUE, jsonRequest.getBytes(UTF_8));
+
+        given(foodTruckService.createFoodTruck(any(CreateFoodTruckDto.class), anyString(), any(MultipartFile.class)))
+                .willReturn(response);
+
+        mockMvc.perform(
+                        multipart("/food-truck")
+                                .file(file)
+                                .file(jsonRequestPart)
+                                .header("Authentication", "authentication")
+                                .contentType(MediaType.MULTIPART_FORM_DATA_VALUE)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andDo(document("create-food-truck",
+                        preprocessResponse(prettyPrint()),
+                        requestParts(
+                                partWithName("file").description("푸드트럭 이미지"),
+                                partWithName("request").description("푸드트럭 정보")
+                        ),
+                        requestPartFields("request",
+                                fieldWithPath("categoryId").type(JsonFieldType.NUMBER)
+                                        .description("카테고리 식별키"),
+                                fieldWithPath("foodTruckName").type(JsonFieldType.STRING)
+                                        .description("푸드트럭 이름"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING)
+                                        .description("연락처"),
+                                fieldWithPath("content").type(JsonFieldType.STRING)
+                                        .description("가게 소개"),
+                                fieldWithPath("originInfo").type(JsonFieldType.STRING)
+                                        .description("원산지 정보"),
+                                fieldWithPath("prepareTime").type(JsonFieldType.NUMBER)
+                                        .description("예상 준비 시간"),
+                                fieldWithPath("waitLimits").type(JsonFieldType.NUMBER)
+                                        .description("최대 주문 대기 건수")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data").type(JsonFieldType.OBJECT)
+                                        .description("응답데이터"),
+                                fieldWithPath("data.foodTruckId").type(JsonFieldType.NUMBER)
+                                        .description("푸드트럭 식별키"),
+                                fieldWithPath("data.categoryId").type(JsonFieldType.NUMBER)
+                                        .description("카테고리 식별키"),
+                                fieldWithPath("data.foodTruckName").type(JsonFieldType.STRING)
+                                        .description("푸드트럭 이름"),
+                                fieldWithPath("data.categoryName").type(JsonFieldType.STRING)
+                                        .description("카테고리 이름"),
+                                fieldWithPath("data.phoneNumber").type(JsonFieldType.STRING)
+                                        .description("연락처"),
+                                fieldWithPath("data.content").type(JsonFieldType.STRING)
+                                        .description("가게 소개"),
+                                fieldWithPath("data.originInfo").type(JsonFieldType.STRING)
+                                        .description("원산지 정보"),
+                                fieldWithPath("data.prepareTime").type(JsonFieldType.NUMBER)
+                                        .description("예상 준비 시간"),
+                                fieldWithPath("data.waitLimits").type(JsonFieldType.NUMBER)
+                                        .description("최대 주문 대기 건수")
+                        )
+                ));
+    }
+}

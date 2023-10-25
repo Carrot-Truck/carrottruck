1f9c562f2cf7794fd77108f2e789c342849786b4
longBright <longbright0804@gmail.com>
2023-10-25 21:57:42 +0900

    feat: 푸드트럭 검색 결과 목록 조회 API 스켈레톤 작성 - FoodTruckController#getFoodTrucks - FoodTruckQueryService#getFoodTrucks - FoodTruckResponse - FoodTruckItem
    Closes #S09P31C211-149

--- Backend/src/main/java/com/boyworld/carrot/api/controller/foodtruck/FoodTruckController.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/foodtruck/FoodTruckController.java
@@ -2,6 +2,8 @@ package com.boyworld.carrot.api.controller.foodtruck;
 
 import com.boyworld.carrot.api.ApiResponse;
 import com.boyworld.carrot.api.controller.foodtruck.request.CreateFoodTruckRequest;
+import com.boyworld.carrot.api.controller.foodtruck.response.FoodTruckResponse;
+import com.boyworld.carrot.api.service.foodtruck.FoodTruckQueryService;
 import com.boyworld.carrot.api.service.foodtruck.FoodTruckService;
 import com.boyworld.carrot.security.SecurityUtil;
 import jakarta.validation.Valid;
@@ -23,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FoodTruckController {
 
     private final FoodTruckService foodTruckService;
+    private final FoodTruckQueryService foodTruckQueryService;
 
     /**
      * 푸드트럭 등록 API
@@ -31,7 +34,7 @@ public class FoodTruckController {
      * @param file    푸드트럭 이미지
      * @return 등록된 푸드트럭 식별키
      */
-    @PostMapping
+    @PostMapping("/vendor")
     @ResponseStatus(HttpStatus.CREATED)
     public ApiResponse<Long> createFoodTruck(@Valid @RequestPart(name = "request") CreateFoodTruckRequest request,
                                              @RequestPart(required = false, name = "file") MultipartFile file) {
@@ -48,4 +51,26 @@ public class FoodTruckController {
         return ApiResponse.created(saveId);
     }
 
+    /**
+     * 푸드트럭 검색 결과 목록 조회 API (사용자)
+     *
+     * @param categoryId      선택한 카테고리 식별키
+     * @param keyword         검색한 푸드트럭/메뉴 이름
+     * @param lastFoodTruckId 마지막으로 조회된 푸드트럭 식별키
+     * @return 푸드트럭 검색 결과 목록
+     */
+    @GetMapping("/client")
+    public ApiResponse<FoodTruckResponse> getFoodTrucks(@RequestParam(required = false, defaultValue = "") String categoryId,
+                                                        @RequestParam(required = false, defaultValue = "") String keyword,
+                                                        @RequestParam(required = false, defaultValue = "") String lastFoodTruckId) {
+        log.debug("FoodTruckController#getFoodTrucks called");
+        log.debug("categoryId={}", categoryId);
+        log.debug("keyword={}", keyword);
+        log.debug("lastFoodTruckId={}", lastFoodTruckId);
+
+        FoodTruckResponse response = foodTruckQueryService.getFoodTrucks(categoryId, keyword, lastFoodTruckId);
+        log.debug("FoodTruckResponse={}", response);
+
+        return ApiResponse.ok(response);
+    }
 }
--- Backend/src/main/java/com/boyworld/carrot/api/controller/foodtruck/response/FoodTruckResponse.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/foodtruck/response/FoodTruckResponse.java
@@ -3,29 +3,18 @@ package com.boyworld.carrot.api.controller.foodtruck.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class FoodTruckResponse {
 
-    private Long foodTruckId;
-    private Long categoryId;
-    private String foodTruckName;
-    private String categoryName;
-    private String phoneNumber;
-    private String content;
-    private String originInfo;
-    private Integer prepareTime;
-    private int waitLimits;
+    private Boolean hasNext;
+    private List<FoodTruckItem> foodTruckItems = new ArrayList<>();
 
     @Builder
-    public FoodTruckResponse(Long foodTruckId, Long categoryId, String foodTruckName, String categoryName, String phoneNumber, String content, String originInfo, Integer prepareTime, int waitLimits) {
-        this.foodTruckId = foodTruckId;
-        this.categoryId = categoryId;
-        this.foodTruckName = foodTruckName;
-        this.categoryName = categoryName;
-        this.phoneNumber = phoneNumber;
-        this.content = content;
-        this.originInfo = originInfo;
-        this.prepareTime = prepareTime;
-        this.waitLimits = waitLimits;
+    public FoodTruckResponse(Boolean hasNext, List<FoodTruckItem> foodTruckItems) {
+        this.hasNext = hasNext;
+        this.foodTruckItems = foodTruckItems;
     }
 }

89eb9385a21f65ccedc8ae58c523caa890af307a
최영환 <longbright0804@gmail.com>
2023-10-27 10:10:57 +0900

    feat: 메뉴 조회 API 스켈레톤 작성 - MenuController#getMenus - MenuQueryService#getMenus - MenuResponse
    Closes #S09P31C211-153

--- Backend/src/main/java/com/boyworld/carrot/api/controller/menu/MenuController.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/menu/MenuController.java
@@ -3,6 +3,7 @@ package com.boyworld.carrot.api.controller.menu;
 import com.boyworld.carrot.api.ApiResponse;
 import com.boyworld.carrot.api.controller.menu.request.CreateMenuRequest;
 import com.boyworld.carrot.api.controller.menu.response.CreateMenuResponse;
+import com.boyworld.carrot.api.service.menu.MenuQueryService;
 import com.boyworld.carrot.api.service.menu.MenuService;
 import com.boyworld.carrot.security.SecurityUtil;
 import jakarta.validation.Valid;
@@ -23,9 +24,10 @@ import org.springframework.web.bind.annotation.*;
 public class MenuController {
 
     private final MenuService menuService;
+    private final MenuQueryService menuQueryService;
 
     /**
-     * 메뉴 등록 API
+     * 푸드트럭 메뉴 등록 API
      *
      * @param request 등록 메뉴 정보
      * @return 등록된 메뉴 정보
@@ -43,5 +45,25 @@ public class MenuController {
         log.debug("CreateMenuResponse={}", response);
 
         return ApiResponse.created(response);
+    }
+
+    /**
+     * 푸드트럭 메뉴 목록 조회
+     *
+     * @param foodTruckId 루드트럭 식별키
+     * @param lastMenuId  마지막으로 조회된 메뉴 식별키
+     * @return 해당 푸드트럭의 메뉴 목록
+     */
+    @GetMapping
+    public ApiResponse<MenuResponse> getMenus(@RequestParam Long foodTruckId,
+                                              @RequestParam(required = false, defaultValue = "0") Long lastMenuId) {
+        log.debug("MenuController#getMenus called");
+        log.debug("foodTruckId={}", foodTruckId);
+        log.debug("lastMenuId={}", lastMenuId);
+
+        MenuResponse response = menuQueryService.getMenus(foodTruckId, lastMenuId);
+        log.debug("MenuResponse={}", response);
+
+        return ApiResponse.ok(response);
     }
 }
---
+++ Backend/src/main/java/com/boyworld/carrot/api/service/menu/MenuQueryService.java
@@ -0,0 +1,33 @@
+package com.boyworld.carrot.api.service.menu;
+
+import com.boyworld.carrot.api.controller.menu.MenuResponse;
+import com.boyworld.carrot.domain.menu.repository.MenuQueryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 메뉴 조회 서비스
+ *
+ * @author 최영환
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MenuQueryService {
+
+    private final MenuQueryRepository menuQueryRepository;
+
+    /**
+     * 푸드트럭 메뉴 목록 조회
+     *
+     * @param foodTruckId 루드트럭 식별키
+     * @param lastMenuId  마지막으로 조회된 메뉴 식별키
+     * @return 해당 푸드트럭의 메뉴 목록
+     */
+    public MenuResponse getMenus(Long foodTruckId, Long lastMenuId) {
+        return null;
+    }
+}

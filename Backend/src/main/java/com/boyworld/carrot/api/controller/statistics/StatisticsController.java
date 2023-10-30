70c3db64d92ca7b4395a4462252632aaef45ceaf
양진형 <jinhyugn@gmail.com>
2023-10-30 09:37:13 +0900

    feat: 매출통계 Controller, Service 샘플 코드 작성

--- Backend/src/main/java/com/boyworld/carrot/api/controller/statistics/StatisticsController.java
+++ Backend/src/main/java/com/boyworld/carrot/api/controller/statistics/StatisticsController.java
@@ -1,47 +1,156 @@
 package com.boyworld.carrot.api.controller.statistics;
 
 import com.boyworld.carrot.api.ApiResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsByPeriodDetailsResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsByPeriodResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsBySalesDetailsResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsBySalesResponse;
+import com.boyworld.carrot.api.controller.statistics.response.*;
 import com.boyworld.carrot.api.service.statistics.StatisticsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 /**
  * 매출통계 API
- * 
+ *
  * @author 양진형
  */
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/statistics")
+@RequestMapping("/statistics/{foodTruckId}")
 public class StatisticsController {
 
     private final StatisticsService statisticsService;
 
-    public ApiResponse<StatisticsBySalesResponse> getStatisticsBySales() {
+    /**
+     * 영업 매출 통계 리스트 API
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 연도
+     * @param page 페이지
+     * @return 영업별 매출 통계 요약 리스트
+     */
+    @GetMapping("/sales")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsBySalesResponse> getStatisticsBySales(@PathVariable Long foodTruckId,
+                                                                       @RequestParam Integer year,
+                                                                       @RequestParam(defaultValue = "1") Integer page) {
+        log.debug("StatisticsController#getStatisticsBySales called !!!");
+        log.debug("FoodTruckID={}, year={}, Page={}", foodTruckId, year, page);
+
+        StatisticsBySalesResponse response = statisticsService.getStatisticsBySales(foodTruckId, year, page);
+        log.debug("StatisticsBySalesResponse={}", response);
 
         return ApiResponse.ok(null);
     }
 
-    public ApiResponse<StatisticsBySalesDetailsResponse> getStatisticsBySalesDetails() {
+    /**
+     * 영업 매출 통계 상세 API
+     *
+     * @param foodTruckId 푸드트럭 Id
+     * @param salesId 영업 ID
+     * @return 영업 매출 통계 상세 정보
+     */
+    @GetMapping("/sales-detail/{salesId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsBySalesDetailsResponse> getStatisticsBySalesDetails(@PathVariable Long foodTruckId,
+                                                                                     @PathVariable Long salesId) {
+        log.debug("StatisticsController#getStatisticsBySalesDetails called !!!");
+        log.debug("FoodTruckId={}, SalesID={}", foodTruckId, salesId);
+
+        StatisticsBySalesDetailsResponse response = statisticsService.getStatisticsBySalesDetails(salesId);
+        log.debug("StatisticsBySalesDetailsResponse={}", response);
 
         return ApiResponse.ok(null);
     }
 
-    public ApiResponse<StatisticsByPeriodResponse> getStatisticsByPeriod() {
+    /**
+     * 주별 매출 통계 리스트 API
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 연도
+     * @param page 페이지
+     * @return 주별 매출 통계 요약 리스트
+     */
+    @GetMapping("/weekly")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsByWeekResponse> getStatisticsByWeek(@PathVariable Long foodTruckId,
+                                                                     @RequestParam Integer year,
+                                                                     @RequestParam(defaultValue = "1") Integer page) {
+        log.debug("StatisticsController#getStatisticsByWeek called !!!");
+        log.debug("FoodTruckID={}, year={}, Page={}", foodTruckId, year, page);
+
+        StatisticsByWeekResponse response = statisticsService.getStatisticsByWeek(foodTruckId, year, page);
+        log.debug("StatisticsByWeekResponse={}", response);
 
         return ApiResponse.ok(null);
     }
 
-    public ApiResponse<StatisticsByPeriodDetailsResponse> getStatisticsByPeriodDetails() {
+    /**
+     * 주 매출 통계 상세 API
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param startDate 시작일
+     * @param endDate 종료일
+     * @return 주 매출 통계 상세 정보
+     */
+    @GetMapping("/weekly-detail")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsByWeekDetailsResponse> getStatisticsByWeekDetails(@PathVariable Long foodTruckId,
+                                                                                   @RequestParam String startDate,
+                                                                                   @RequestParam String endDate) {
+        log.debug("StatisticsController#getStatisticsByWeekDetails called !!!");
+        log.debug("FoodTruckID={}, startDate={}, endDate={}", foodTruckId, startDate, endDate);
+
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+
+        StatisticsByWeekDetailsResponse response = statisticsService.getStatisticsByWeekDetails(foodTruckId, start, end);
+        log.debug("StatisticsByWeekDetailsResponse={}", response);
 
         return ApiResponse.ok(null);
     }
 
+    /**
+     * 월별 매출 통계 리스트 API
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 연도
+     * @return 월별 매출 통계 요약 리스트
+     */
+    @GetMapping("/monthly")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsByMonthResponse> getStatisticsByMonth(@PathVariable Long foodTruckId,
+                                                                       @RequestParam Integer year) {
+        log.debug("StatisticsController#getStatisticsByMonth called !!!");
+        log.debug("FoodTruckID={}, year={}", foodTruckId, year);
+
+        StatisticsByMonthResponse response = statisticsService.getStatisticsByMonth(foodTruckId, year);
+        log.debug("StatisticsByMonthResponse={}", response);
+
+        return ApiResponse.ok(null);
+    }
+
+    /**
+     * 월 매출 통계 상세 API
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 연도
+     * @param month 월
+     * @return 월 매출 통계 상세 정보
+     */
+    @GetMapping("/monthly-detail")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<StatisticsByMonthDetailsResponse> getStatisticsByMonthDetails(@PathVariable Long foodTruckId,
+                                                                                     @RequestParam Integer year,
+                                                                                     @RequestParam Integer month) {
+        log.debug("StatisticsController#getStatisticsByMonthDetails called !!!");
+        log.debug("FoodTruckID={}, year={}, month={}", foodTruckId, year, month);
+
+        StatisticsByMonthDetailsResponse response = statisticsService.getStatisticsByMonthDetails(foodTruckId, year, month);
+        log.debug("StatisticsByMonthDetailsResponse={}", response);
+
+        return ApiResponse.ok(null);
+    }
 }
--- Backend/src/main/java/com/boyworld/carrot/api/service/statistics/StatisticsService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/statistics/StatisticsService.java
@@ -1,13 +1,12 @@
 package com.boyworld.carrot.api.service.statistics;
 
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsByPeriodDetailsResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsByPeriodResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsBySalesDetailsResponse;
-import com.boyworld.carrot.api.controller.statistics.response.StatisticsBySalesResponse;
+import com.boyworld.carrot.api.controller.statistics.response.*;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 /**
  * 매출통계 서비스
@@ -21,19 +20,78 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class StatisticsService {
 
-    public StatisticsBySalesResponse getStatisticsBySalesList() {
+    /**
+     * 영업별 매출통계 리스트
+     * 연단위 구분
+     * 최신순 정렬
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 조회 연도
+     * @param page 페이지
+     * @return 매출통계 리스트
+     */
+    public StatisticsBySalesResponse getStatisticsBySales(Long foodTruckId, Integer year, Integer page) {
         return null;
     }
 
-    public StatisticsBySalesDetailsResponse getStatisticsBySalesDetail() {
+    /**
+     * 영업 매출통계 상세
+     *
+     * @param salesId 영업 ID
+     * @return 매출통계 상세
+     */
+    public StatisticsBySalesDetailsResponse getStatisticsBySalesDetails(Long salesId) {
         return null;
     }
 
-    public StatisticsByPeriodResponse getStatisticsByWeekList() {
+    /**
+     * 주별 매출통계 리스트
+     * 연단위 구분
+     * 최신순 정렬
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 조회 연도
+     * @param page 페이지
+     * @return 매출통계 리스트
+     */
+    public StatisticsByWeekResponse getStatisticsByWeek(Long foodTruckId, Integer year, Integer page) {
         return null;
     }
 
-    public StatisticsByPeriodDetailsResponse getStatisticsByPeriodDetail() {
+    /**
+     * 주 매출통계 상세
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param startDate 조회 시작 시간
+     * @param endDate 조회 끝 시간
+     * @return 매출통계 상세
+     */
+    public StatisticsByWeekDetailsResponse getStatisticsByWeekDetails(Long foodTruckId, LocalDate startDate, LocalDate endDate) {
+        return null;
+    }
+
+    /**
+     * 월별 매출통계 리스트
+     * 연단위 구분
+     * 최신순 정렬
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 조회 연도
+     * @return 매출통계 리스트
+     */
+    public StatisticsByMonthResponse getStatisticsByMonth(Long foodTruckId, Integer year) {
+        return null;
+    }
+
+    /**
+     * 월 매출통계 상세
+     *
+     * @param foodTruckId 푸드트럭 ID
+     * @param year 년
+     * @param month 월
+     * @return 매출통계 상세
+     */
+    public StatisticsByMonthDetailsResponse getStatisticsByMonthDetails(Long foodTruckId, Integer year, Integer month) {
         return null;
     }
 }

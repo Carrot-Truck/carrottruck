6dba82bbdb7345854a2d771660ac4cf859c57951
양진형 <jinhyugn@gmail.com>
2023-10-30 16:47:39 +0900

    feat: 수요조사 API 작성 (진행중)

--- Backend/src/main/java/com/boyworld/carrot/api/service/survey/SurveyService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/survey/SurveyService.java
@@ -2,13 +2,16 @@ package com.boyworld.carrot.api.service.survey;
 
 import com.boyworld.carrot.api.controller.survey.request.CreateSurveyRequest;
 import com.boyworld.carrot.api.controller.survey.response.CreateSurveyResponse;
-import com.boyworld.carrot.api.controller.survey.response.SurveyDetailsResponse;
-import com.boyworld.carrot.api.controller.survey.response.SurveyCountResponse;
+import com.boyworld.carrot.api.service.GeocodingUtil;
+import com.boyworld.carrot.domain.survey.Survey;
 import com.boyworld.carrot.domain.survey.repository.SurveyRepository;
+import com.boyworld.carrot.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 /**
  * 수요조사 서비스
@@ -30,8 +33,11 @@ public class SurveyService {
      * @return 제출 수요조사 정보
      */
     public CreateSurveyResponse createSurvey(CreateSurveyRequest request) {
+        BigDecimal latitude = request.getLatitude();
+        BigDecimal longitude = request.getLongitude();
 
-        // GeocodingUtil 사용
+        String result = GeocodingUtil.geocoding(latitude, longitude);
+
         return null;
     }
 
@@ -42,7 +48,26 @@ public class SurveyService {
      * @return 삭제한 수요조사 ID
      */
     public Long deleteSurvey(Long surveyId) {
-        return null;
-    }
+        String email = SecurityUtil.getCurrentLoginId();
+        Survey survey = surveyRepository.findById(surveyId).orElse(null);
 
+        if(survey == null) {
+            // 존재하지 않는 수요조사
+            return null;
+        }
+
+        if (!survey.getMember().getEmail().equals(email)) {
+            // 로그인한 회원과 수요조사 작성 회원 불일치
+            return null;
+        }
+
+        if (!survey.getMember().getActive()) {
+            // 탈퇴한 회원이 작성한 수요조사
+            return null;
+        }
+
+        survey.delete();
+
+        return surveyId;
+    }
 }

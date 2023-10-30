59d194d9cac32d12559aa6ee15ec6539091461fc
최영환 <longbright0804@gmail.com>
2023-10-30 11:37:22 +0900

    test: 회원 레포지토리 테스트 - MemberRepository - MemberQueryRepository
    Closes #S09P31C211-204

---
+++ Backend/src/test/java/com/boyworld/carrot/domain/member/repository/MemberQueryRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.boyworld.carrot.domain.member.repository;
+
+import com.boyworld.carrot.IntegrationTestSupport;
+import com.boyworld.carrot.domain.member.Member;
+import com.boyworld.carrot.domain.member.Role;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 회원 조회용 레포지토리 테스트
+ *
+ * @author 최영환
+ */
+@Slf4j
+class MemberQueryRepositoryTest extends IntegrationTestSupport {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberQueryRepository memberQueryRepository;
+
+    @DisplayName("이미 등록된 이메일이라면 true 를 반환한다.")
+    @Test
+    void isExistEmailWithTrue() {
+        Member member = createMember();
+
+        String email = "ssafy@ssafy.com";
+
+        Boolean result = memberQueryRepository.isExistEmail(email);
+
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("이미 등록되지 않은 이메일이라면 false 를 반환한다.")
+    @Test
+    void isExistEmailWithFalse() {
+        Member member = createMember();
+
+        String email = "ssafy@naver.com";
+
+        Boolean result = memberQueryRepository.isExistEmail(email);
+
+        assertThat(result).isFalse();
+    }
+
+    private Member createMember() {
+        Member member = Member.builder()
+                .email("ssafy@ssafy.com")
+                .nickname("매미킴")
+                .name("김동현")
+                .phoneNumber("010-1234-5678")
+                .role(Role.CLIENT)
+                .active(true)
+                .build();
+        return memberRepository.save(member);
+    }
+}

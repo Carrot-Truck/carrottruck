6b6e04154fbbb0e5a736f91eddd034656522df5b
최영환 <longbright0804@gmail.com>
2023-10-30 17:23:29 +0900

    feat: 회원 정보 조회 쿼리 구현 - MemberQueryRepository#getClientInfoByEmail - MemberQueryRepository#getVendorInfoByEmail
    Closes #S09P31C211-205

--- Backend/src/main/java/com/boyworld/carrot/domain/member/repository/MemberQueryRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/member/repository/MemberQueryRepository.java
@@ -1,12 +1,16 @@
 package com.boyworld.carrot.domain.member.repository;
 
+import com.boyworld.carrot.api.controller.member.response.ClientResponse;
+import com.boyworld.carrot.api.controller.member.response.VendorResponse;
 import com.boyworld.carrot.domain.member.Role;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import static com.boyworld.carrot.domain.member.QMember.member;
+import static com.boyworld.carrot.domain.member.QVendorInfo.vendorInfo;
 import static org.springframework.util.StringUtils.hasText;
 
 /**
@@ -37,6 +41,53 @@ public class MemberQueryRepository {
                 )
                 .fetchFirst();
         return result != null;
+    }
+
+    /**
+     * email 로 일반 사용자 정보 조회
+     *
+     * @param email 현재 로그인 중인 회원 이메일
+     * @return email 에 해당하는 사용자 정보
+     */
+    public ClientResponse getClientInfoByEmail(String email) {
+        return queryFactory
+                .select(Projections.constructor(ClientResponse.class,
+                        member.name,
+                        member.nickname,
+                        member.email,
+                        member.phoneNumber,
+                        member.role
+                ))
+                .from(member)
+                .where(
+                        isEqualEmail(email),
+                        isEqualRole(Role.CLIENT.toString())
+                )
+                .fetchOne();
+    }
+
+    /**
+     * 이메일로 사업자 정보 조회 쿼리
+     *
+     * @param email 현재 로그인 중인 회원 이메일
+     * @return 이메일에 해당하는 사업자 정보
+     */
+    public VendorResponse getVendorInfoByEmail(String email) {
+        return queryFactory.select(Projections.constructor(VendorResponse.class,
+                        member.name,
+                        member.nickname,
+                        member.email,
+                        member.phoneNumber,
+                        vendorInfo.businessNumber,
+                        member.role
+                ))
+                .from(member)
+                .join(vendorInfo.member, member)
+                .where(
+                        isEqualEmail(email),
+                        isEqualRole(Role.VENDOR.toString())
+                )
+                .fetchOne();
     }
 
     private BooleanExpression isEqualEmail(String email) {

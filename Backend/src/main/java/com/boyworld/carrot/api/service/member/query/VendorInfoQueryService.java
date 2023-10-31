1077ea2d412b687fb38fb4b689775446ec753da8
최영환 <longbright0804@gmail.com>
2023-10-31 15:38:21 +0900

    feat: 사업자 정보 조회 서비스 구현
    Closes #S09P31C211-207

--- Backend/src/main/java/com/boyworld/carrot/api/service/member/query/VendorInfoQueryService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/member/query/VendorInfoQueryService.java
@@ -1,12 +1,13 @@
 package com.boyworld.carrot.api.service.member.query;
 
 import com.boyworld.carrot.api.controller.member.response.VendorInfoResponse;
-import com.boyworld.carrot.domain.member.repository.command.VendorInfoRepository;
 import com.boyworld.carrot.domain.member.repository.query.VendorInfoQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 /**
  * 사업자 정보 조회 서비스
@@ -19,7 +20,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class VendorInfoQueryService {
 
-    private final VendorInfoRepository vendorInfoRepository;
     private final VendorInfoQueryRepository vendorInfoQueryRepository;
 
     /**
@@ -29,6 +29,7 @@ public class VendorInfoQueryService {
      * @return 현재 로그인한 사용자의 사업자 정보
      */
     public VendorInfoResponse getVendorInfo(String email) {
-        return null;
+        return vendorInfoQueryRepository.getVendorInfoByEmail(email)
+                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 사업자 정보입니다."));
     }
 }

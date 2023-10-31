bfeae48b5601d00987962d008fd6e362fb28eaa9
박은규 <parkeg1223@gmail.com>
2023-10-31 17:59:16 +0900

    fix: Data annotation 추가

--- Backend/src/main/java/com/boyworld/carrot/api/service/order/dto/OrderItem.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/order/dto/OrderItem.java
@@ -2,7 +2,12 @@ package com.boyworld.carrot.api.service.order.dto;
 
 import com.boyworld.carrot.domain.order.Status;
 import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class OrderItem {
 
     private Long orderId;
@@ -11,4 +16,12 @@ public class OrderItem {
     private LocalDateTime expectTime;
     private Integer totalPrice;
 
+    @Builder
+    public OrderItem(Long orderId, Status status, LocalDateTime createdTime, LocalDateTime expectTime, Integer totalPrice) {
+        this.orderId = orderId;
+        this.status = status;
+        this.createdTime = createdTime;
+        this.expectTime = expectTime;
+        this.totalPrice = totalPrice;
+    }
 }

d8275a0bdb36be46edf8564e6dd0e4c5d74a6bd9
최영환 <longbright0804@gmail.com>
2023-10-31 10:55:04 +0900

    feat: 회원 주소 목록 조회 서비스 구현 - AccountService#getMemberAddresses - MemberAddressResponse#of
    Closes #S09P31C211-206

--- Backend/src/main/java/com/boyworld/carrot/api/service/member/AccountService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/member/AccountService.java
@@ -1,15 +1,20 @@
 package com.boyworld.carrot.api.service.member;
 
 import com.boyworld.carrot.api.controller.member.response.ClientResponse;
+import com.boyworld.carrot.api.controller.member.response.MemberAddressDetailResponse;
 import com.boyworld.carrot.api.controller.member.response.MemberAddressResponse;
 import com.boyworld.carrot.api.controller.member.response.VendorResponse;
+import com.boyworld.carrot.domain.member.repository.MemberAddressQueryRepository;
 import com.boyworld.carrot.domain.member.repository.MemberQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.NoSuchElementException;
+
+import static com.boyworld.carrot.domain.SizeConstants.PAGE_SIZE;
 
 /**
  * 계정 관련 서비스
@@ -23,6 +28,8 @@ import java.util.NoSuchElementException;
 public class AccountService {
 
     private final MemberQueryRepository memberQueryRepository;
+
+    private final MemberAddressQueryRepository memberAddressQueryRepository;
 
     /**
      * 로그인 중인 일반 사용자 회원 정보 조회
@@ -68,6 +75,38 @@ public class AccountService {
      * @return 로그인 중인 사용자의 주소 목록
      */
     public MemberAddressResponse getMemberAddresses(String email, String lastMemberAddressId) {
-        return null;
+        List<MemberAddressDetailResponse> memberAddresses = memberAddressQueryRepository
+                .getMemberAddressesByEmail(email, getLastMemberAddressId(lastMemberAddressId));
+
+        boolean hasNext = checkHasNext(memberAddresses);
+
+        return MemberAddressResponse.of(memberAddresses, hasNext);
+    }
+
+    /**
+     * 마지막으로 조회된 회원 주소 식별키 변환
+     *
+     * @param lastMemberAddressId 마지막으로 조회된 주소 식별키
+     * @return Long 타입으로 변환된 식별키 값
+     */
+    private Long getLastMemberAddressId(String lastMemberAddressId) {
+        if (lastMemberAddressId.isBlank()) {
+            return 0L;
+        }
+        return Long.parseLong(lastMemberAddressId);
+    }
+
+    /**
+     * 다음 페이지 존재여부 확인
+     *
+     * @param memberAddresses 회원주소 리스트
+     * @return true: 다음 페이지가 있는 경우 / false: 다음 페이지가 없는 경우
+     */
+    private boolean checkHasNext(List<MemberAddressDetailResponse> memberAddresses) {
+        if (memberAddresses.size() > PAGE_SIZE) {
+            memberAddresses.remove(PAGE_SIZE);
+            return true;
+        }
+        return false;
     }
 }

98a21932432959a088634f5d5cdefdc703e462a7
최영환 <longbright0804@gmail.com>
2023-10-31 10:56:26 +0900

    test: 회원 주소 목록 조회 서비스 테스트
    Closes #S09P31C211-206

--- Backend/src/test/java/com/boyworld/carrot/api/service/member/AccountServiceTest.java
+++ Backend/src/test/java/com/boyworld/carrot/api/service/member/AccountServiceTest.java
@@ -3,13 +3,13 @@ package com.boyworld.carrot.api.service.member;
 
 import com.boyworld.carrot.IntegrationTestSupport;
 import com.boyworld.carrot.api.controller.member.response.ClientResponse;
+import com.boyworld.carrot.api.controller.member.response.MemberAddressResponse;
 import com.boyworld.carrot.api.controller.member.response.VendorResponse;
 import com.boyworld.carrot.domain.member.Member;
+import com.boyworld.carrot.domain.member.MemberAddress;
 import com.boyworld.carrot.domain.member.Role;
 import com.boyworld.carrot.domain.member.VendorInfo;
-import com.boyworld.carrot.domain.member.repository.MemberQueryRepository;
-import com.boyworld.carrot.domain.member.repository.MemberRepository;
-import com.boyworld.carrot.domain.member.repository.VendorInfoRepository;
+import com.boyworld.carrot.domain.member.repository.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,8 +39,13 @@ class AccountServiceTest extends IntegrationTestSupport {
     private VendorInfoRepository vendorInfoRepository;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private MemberAddressRepository memberAddressRepository;
 
+    @Autowired
+    private MemberAddressQueryRepository memberAddressQueryRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private MemberQueryRepository memberQueryRepository;
@@ -134,6 +139,40 @@ class AccountServiceTest extends IntegrationTestSupport {
                 .hasMessage("존재하지 않는 회원입니다.");
     }
 
+    @DisplayName("모든 사용자는 등록된 회원 주소 목록을 조회할 수 있다.")
+    @Test
+    void getMemberAddresses() {
+        // given
+        Member member = createMember(Role.CLIENT);
+        MemberAddress memberAddress1 = createMemberAddress(member, "주소1");
+        MemberAddress memberAddress2 = createMemberAddress(member, "주소2");
+
+        // when
+        MemberAddressResponse response = accountService.getMemberAddresses(member.getEmail(), "");
+        log.debug("response={}", response);
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getHasNext()).isFalse();
+        assertThat(response.getMemberAddresses()).hasSize(2);
+    }
+
+    @DisplayName("등록된 주소가 없으면 빈 리스트가 반환된다.")
+    @Test
+    void getEmptyMemberAddresses() {
+        // given
+        Member member = createMember(Role.CLIENT);
+
+        // when
+        MemberAddressResponse response = accountService.getMemberAddresses(member.getEmail(), "");
+        log.debug("response={}", response);
+
+        // then
+        assertThat(response).isNotNull();
+        assertThat(response.getHasNext()).isFalse();
+        assertThat(response.getMemberAddresses()).isEmpty();
+    }
+
     private Member createMember(Role role) {
         Member member = Member.builder()
                 .email("ssafy@ssafy.com")
@@ -157,5 +196,14 @@ class AccountServiceTest extends IntegrationTestSupport {
                 .active(true)
                 .build();
         return vendorInfoRepository.save(vendorInfo);
+    }
+
+    private MemberAddress createMemberAddress(Member member, String address) {
+        MemberAddress memberAddress = MemberAddress.builder()
+                .member(member)
+                .address(address)
+                .active(true)
+                .build();
+        return memberAddressRepository.save(memberAddress);
     }
 }

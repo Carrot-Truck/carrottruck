7bf79796c9078730539e04ba2212881a8097c75f
김동현 <qhfhdls12@naver.com>
2023-11-02 01:33:45 +0900

    feat: 장바구니 조회 서비스 구현
    - CartService의 getCart 생성

--- Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
@@ -1,6 +1,5 @@
 package com.boyworld.carrot.api.service.cart;
 
-import com.boyworld.carrot.api.controller.cart.response.CartResponse;
 import com.boyworld.carrot.api.service.cart.dto.CreateCartMenuDto;
 import com.boyworld.carrot.domain.cart.repository.RedisCartRepository;
 import jakarta.transaction.Transactional;
@@ -16,10 +15,6 @@ public class CartService {
     private final RedisCartRepository redisCartRepository;
 
     public Long createCart(CreateCartMenuDto createCartMenuDto, String email) {
-        return null;
-    }
-
-    public CartResponse getCart(String email) {
         return null;
     }
 }

47a8ca34d90baf9c5620e29d26ce6f0b900940b0
양진형 <jinhyugn@gmail.com>
2023-11-01 09:48:27 +0900

    feat: 상권분석 컨트롤러 테스트 코드 작성

--- Backend/src/test/java/com/boyworld/carrot/docs/analysis/AnalysisControllerDocsTest.java
+++ Backend/src/test/java/com/boyworld/carrot/docs/analysis/AnalysisControllerDocsTest.java
@@ -11,8 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.math.BigDecimal;
+import java.util.List;
 
-import static jdk.internal.org.jline.reader.impl.LineReaderImpl.CompletionType.List;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
@@ -37,15 +37,71 @@ public class AnalysisControllerDocsTest extends RestDocsSupport {
     void getStoreAnalysis() throws Exception {
 
         StoreAnalysisDto item1 = StoreAnalysisDto.builder()
-                .storeName("")
-                .latitude(new BigDecimal("35.19684"))
-                .longitude(new BigDecimal("126.8108"))
+                .storeName("빨간곱창")
+                .latitude(new BigDecimal("126.830916831796"))
+                .longitude(new BigDecimal("35.1918574116446"))
+                .sido("광주광역시")
+                .sigungu("광산구")
+                .dong("수완동")
+                .middleClassCode("I201")
+                .middleClassName("한식")
+                .smallClassCode("I20109")
+                .smallClassName("곱창 전골/구이")
+                .build();
+
+        StoreAnalysisDto item2 = StoreAnalysisDto.builder()
+                .storeName("국수나무장덕점")
+                .latitude(new BigDecimal("126.814597161796"))
+                .longitude(new BigDecimal("35.1950879287079"))
                 .sido("광주광역시")
                 .sigungu("광산구")
                 .dong("장덕동")
                 .middleClassCode("I201")
-                .smallClassCode("I20109")
+                .middleClassName("한식")
+                .smallClassCode("I20105")
+                .smallClassName("국수/칼국수")
                 .build();
+
+        StoreAnalysisDto item3 = StoreAnalysisDto.builder()
+                .storeName("호야명품")
+                .latitude(new BigDecimal("126.815120825025"))
+                .longitude(new BigDecimal("35.1915416402982"))
+                .sido("광주광역시")
+                .sigungu("광산구")
+                .dong("장덕동")
+                .middleClassCode("I201")
+                .middleClassName("한식")
+                .smallClassCode("I20101")
+                .smallClassName("백반/한정식")
+                .build();
+
+        StoreAnalysisDto item4 = StoreAnalysisDto.builder()
+                .storeName("예향정")
+                .latitude(new BigDecimal("126.81277113399"))
+                .longitude(new BigDecimal("35.191536654724"))
+                .sido("광주광역시")
+                .sigungu("광산구")
+                .dong("장덕동")
+                .middleClassCode("I201")
+                .middleClassName("한식")
+                .smallClassCode("I20101")
+                .smallClassName("백반/한정식")
+                .build();
+
+        StoreAnalysisDto item5 = StoreAnalysisDto.builder()
+                .storeName("백두산")
+                .latitude(new BigDecimal("126.817929899939"))
+                .longitude(new BigDecimal("35.1833305240406"))
+                .sido("광주광역시")
+                .sigungu("광산구")
+                .dong("신가동")
+                .middleClassCode("I201")
+                .middleClassName("한식")
+                .smallClassCode("I20101")
+                .smallClassName("백반/한정식")
+                .build();
+
+        List<StoreAnalysisDto> items = List.of(item1, item2, item3, item4, item5);
 
         StoreAnalysisResponse response = StoreAnalysisResponse.builder()
                 .categoryId(1L)
@@ -53,9 +109,80 @@ public class AnalysisControllerDocsTest extends RestDocsSupport {
                 .sido("광주광역시")
                 .sigungu("광산구")
                 .dong("수완동")
-                .radiusCount(4)
-                .addressCount(15)
-                .stores(List.of(null));
+                .radiusCount(115)
+                .addressCount(390)
+                .stores(items)
+                .build();
+
+        given(analysisQueryService.getStoreAnalysis(anyInt(), any(BigDecimal.class), any(BigDecimal.class)))
+                .willReturn(response);
+
+        Long cid = 1L;
+
+        mockMvc.perform(
+                get("/analysis/store/{categoryId}", cid)
+                        .param("latitude", "35.19684")
+                        .param("longitude", "126.8108")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("get-analysis-store-category",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        pathParameters(
+                                parameterWithName("categoryId")
+                                        .description("카테고리 ID")
+                        ),
+                        queryParameters(
+                                parameterWithName("latitude")
+                                        .description("현재 위도 위치"),
+                                parameterWithName("longitude")
+                                        .description("현재 경도 위치")
+                        ),
+                        responseFields(
+                                fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                        .description("코드"),
+                                fieldWithPath("status").type(JsonFieldType.STRING)
+                                        .description("상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data.categoryId").type(JsonFieldType.NUMBER)
+                                        .description("카테고리 ID"),
+                                fieldWithPath("data.categoryName").type(JsonFieldType.STRING)
+                                        .description("카테고리 이름"),
+                                fieldWithPath("data.sido").type(JsonFieldType.STRING)
+                                        .description("분석한 시도"),
+                                fieldWithPath("data.sigungu").type(JsonFieldType.STRING)
+                                        .description("분석한 시군구"),
+                                fieldWithPath("data.dong").type(JsonFieldType.STRING)
+                                        .description("분석한 읍면동(법정동)"),
+                                fieldWithPath("data.radiusCount").type(JsonFieldType.NUMBER)
+                                        .description("반경 범위 내 상가업소 수"),
+                                fieldWithPath("data.addressCount").type(JsonFieldType.NUMBER)
+                                        .description("동일 행정동 내 상가업소 수"),
+                                fieldWithPath("data.stores[].storeName").type(JsonFieldType.STRING)
+                                        .description("상가업소의 상호명"),
+                                fieldWithPath("data.stores[].latitude").type(JsonFieldType.NUMBER)
+                                        .description("상가업소의 위도 좌표값"),
+                                fieldWithPath("data.stores[].longitude").type(JsonFieldType.NUMBER)
+                                        .description("상가업소의 경도 좌표값"),
+                                fieldWithPath("data.stores[].sido").type(JsonFieldType.STRING)
+                                        .description("상가업소의 시도 명칭"),
+                                fieldWithPath("data.stores[].sigungu").type(JsonFieldType.STRING)
+                                        .description("상가업소의 시군구 명칭"),
+                                fieldWithPath("data.stores[].dong").type(JsonFieldType.STRING)
+                                        .description("상가업소의 읍면동 명칭(법정동)"),
+                                fieldWithPath("data.stores[].middleClassCode").type(JsonFieldType.STRING)
+                                        .description("상권업종중분류코드(4자리)"),
+                                fieldWithPath("data.stores[].middleClassName").type(JsonFieldType.STRING)
+                                        .description("상권업종중분류명"),
+                                fieldWithPath("data.stores[].smallClassCode").type(JsonFieldType.STRING)
+                                        .description("상권업종소분류코드(6자리)"),
+                                fieldWithPath("data.stores[].smallClassName").type(JsonFieldType.STRING)
+                                        .description("상권업종소분류명")
+                        )
+                ));
+
     }
 
 }

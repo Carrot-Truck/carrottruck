6dd0394944d67798abd84d631917b3c27c9d713b
longBright <longbright0804@gmail.com>
2023-11-02 00:25:10 +0900

    feat: 보유 푸드트럭 목록 조회 쿼리 구현 & 선택된 푸드트럭 개수 조회 쿼리 버그 수정
    Closes #S09P31C211-209

--- Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/FoodTruckQueryRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/FoodTruckQueryRepository.java
@@ -1,9 +1,14 @@
 package com.boyworld.carrot.domain.foodtruck.repository.query;
 
+import com.boyworld.carrot.api.controller.foodtruck.response.FoodTruckOverview;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.boyworld.carrot.domain.foodtruck.QFoodTruck.foodTruck;
 import static com.boyworld.carrot.domain.member.QMember.member;
@@ -27,7 +32,7 @@ public class FoodTruckQueryRepository {
      */
     public Long getSelectedCountByEmail(String email) {
         return queryFactory
-                .select(foodTruck.id)
+                .select(foodTruck.count())
                 .from(foodTruck)
                 .join(foodTruck.vendor, member)
                 .where(
@@ -38,8 +43,48 @@ public class FoodTruckQueryRepository {
 
     }
 
+    /**
+     * 이메일로 보유 푸드트럭 조회 쿼리
+     *
+     * @param lastFoodTruckId 마지막으로 조회된 푸드트럭 식별키
+     * @param email           사업자 이메일
+     * @return 해당 사업자 보유 푸드트럭 목록
+     */
+    public List<FoodTruckOverview> getFoodTruckOverviewsByEmail(Long lastFoodTruckId, String email) {
+        List<Long> ids = queryFactory
+                .select(foodTruck.id)
+                .from(foodTruck)
+                .join(foodTruck.vendor, member)
+                .where(
+                        isEqualEmail(email),
+                        isGreaterThanLastId(lastFoodTruckId),
+                        isActiveFoodTruck()
+                )
+                .fetch();
+
+        if (ids == null || ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+                .select(Projections.constructor(FoodTruckOverview.class,
+                        foodTruck.id,
+                        foodTruck.name,
+                        foodTruck.selected
+                ))
+                .from(foodTruck)
+                .where(
+                        foodTruck.id.in(ids)
+                )
+                .fetch();
+    }
+
     private BooleanExpression isEqualEmail(String email) {
         return hasText(email) ? foodTruck.vendor.email.eq(email) : null;
+    }
+
+    private BooleanExpression isGreaterThanLastId(Long lastFoodTruckId) {
+        return lastFoodTruckId != null ? foodTruck.id.gt(lastFoodTruckId) : null;
     }
 
     private static BooleanExpression isActiveFoodTruck() {

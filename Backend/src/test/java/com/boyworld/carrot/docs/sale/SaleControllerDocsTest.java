a32f01bfe570f0d90a6db5fcc4df56d061788091
박은규 <parkeg1223@gmail.com>
2023-11-01 17:52:23 +0900

    feat: 영업 테스트코드 작성

--- Backend/src/test/java/com/boyworld/carrot/docs/sale/SaleControllerDocsTest.java
+++ Backend/src/test/java/com/boyworld/carrot/docs/sale/SaleControllerDocsTest.java
@@ -1,11 +1,14 @@
 package com.boyworld.carrot.docs.sale;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -15,17 +18,21 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.response
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.boyworld.carrot.api.controller.order.response.OrderResponse;
+import com.boyworld.carrot.api.controller.order.response.ClientOrderResponse;
+import com.boyworld.carrot.api.controller.order.response.VendorOrderResponse;
 import com.boyworld.carrot.api.controller.sale.SaleController;
+import com.boyworld.carrot.api.controller.sale.request.AcceptOrderRequest;
+import com.boyworld.carrot.api.controller.sale.request.DeclineOrderRequest;
 import com.boyworld.carrot.api.controller.sale.request.OpenSaleRequest;
+import com.boyworld.carrot.api.controller.sale.response.CloseSaleResponse;
 import com.boyworld.carrot.api.controller.sale.response.OpenSaleResponse;
 import com.boyworld.carrot.api.service.order.OrderService;
-import com.boyworld.carrot.api.service.order.dto.OrderItem;
+import com.boyworld.carrot.api.service.order.dto.ClientOrderItem;
+import com.boyworld.carrot.api.service.order.dto.VendorOrderItem;
 import com.boyworld.carrot.api.service.sale.SaleService;
 import com.boyworld.carrot.api.service.sale.dto.SaleMenuItem;
 import com.boyworld.carrot.docs.RestDocsSupport;
 import com.boyworld.carrot.domain.order.Status;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -126,8 +133,8 @@ public class SaleControllerDocsTest extends RestDocsSupport {
     @WithMockUser(roles = "VENDOR")
     void getProcessingOrders() throws Exception {
 
-        List<OrderItem> orderItems = new ArrayList<>();
-        orderItems.add(OrderItem.builder()
+        List<VendorOrderItem> vendorOrderItems = new ArrayList<>();
+        vendorOrderItems.add(VendorOrderItem.builder()
             .orderId(1L)
             .status(Status.PROCESSING)
             .createdTime(LocalDateTime.of(2023, 10, 30, 17, 25))
@@ -135,19 +142,19 @@ public class SaleControllerDocsTest extends RestDocsSupport {
             .totalPrice(10000)
             .build()
         );
-        orderItems.add(OrderItem.builder()
+        vendorOrderItems.add(VendorOrderItem.builder()
             .orderId(2L)
-            .status(Status.PROCESSING)
+            .status(Status.PENDING)
             .createdTime(LocalDateTime.of(2023, 10, 30, 17, 35))
             .expectTime(LocalDateTime.of(2023, 10, 30, 18, 5))
             .totalPrice(20000)
             .build()
         );
-        OrderResponse response = OrderResponse.builder()
-            .orderItems(orderItems)
+        VendorOrderResponse response = VendorOrderResponse.builder()
+            .vendorOrderItems(vendorOrderItems)
             .build();
 
-        given(orderService.getProcessingOrders(1L, "ssafy@ssafy.com"))
+        given(orderService.getProcessingOrders(anyLong(), anyString()))
             .willReturn(response);
 
         mockMvc.perform(
@@ -168,7 +175,7 @@ public class SaleControllerDocsTest extends RestDocsSupport {
                         fieldWithPath("data").type(JsonFieldType.OBJECT)
                             .description("진행 중인 주문"),
                         fieldWithPath("data.orderItems").type(JsonFieldType.ARRAY)
-                            .description("진행 중인 주문"),
+                            .description("진행 중인 주문 정보"),
                         fieldWithPath("data.orderItems[].orderId").type(JsonFieldType.NUMBER)
                             .description("주문 ID"),
                         fieldWithPath("data.orderItems[].status").type(JsonFieldType.STRING)
@@ -184,27 +191,270 @@ public class SaleControllerDocsTest extends RestDocsSupport {
             );
     }
 
+    @DisplayName("완료된 주문 조회 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void getCompleteOrders() throws Exception {
 
+        List<VendorOrderItem> vendorOrderItems = new ArrayList<>();
+        vendorOrderItems.add(VendorOrderItem.builder()
+            .orderId(3L)
+            .status(Status.COMPLETE)
+            .createdTime(LocalDateTime.of(2023, 11, 1, 15, 25))
+            .expectTime(LocalDateTime.of(2023, 11, 1, 15, 55))
+            .totalPrice(30000)
+            .build()
+        );
+        vendorOrderItems.add(VendorOrderItem.builder()
+            .orderId(4L)
+            .status(Status.CANCELLED)
+            .createdTime(LocalDateTime.of(2023, 11, 1, 15, 35))
+            .expectTime(null)
+            .totalPrice(40000)
+            .build()
+        );
+        vendorOrderItems.add(VendorOrderItem.builder()
+            .orderId(5L)
+            .status(Status.DECLINED)
+            .createdTime(LocalDateTime.of(2023, 11, 1, 15, 45))
+            .expectTime(null)
+            .totalPrice(50000)
+            .build()
+        );
+        VendorOrderResponse response = VendorOrderResponse.builder()
+            .vendorOrderItems(vendorOrderItems)
+            .build();
+
+        given(orderService.getCompleteOrders(anyLong(), anyString()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                get("/sale/complete/1")
+                    .header("Authentication", "authentication")
+            ).andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("get-complete-orders",
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.OBJECT)
+                            .description("완료된 주문"),
+                        fieldWithPath("data.orderItems").type(JsonFieldType.ARRAY)
+                            .description("완료된 주문 정보"),
+                        fieldWithPath("data.orderItems[].orderId").type(JsonFieldType.NUMBER)
+                            .description("완료된 ID"),
+                        fieldWithPath("data.orderItems[].status").type(JsonFieldType.STRING)
+                            .description("완료된 상태"),
+                        fieldWithPath("data.orderItems[].createdTime").type(JsonFieldType.ARRAY)
+                            .description("완료된 시각"),
+                        fieldWithPath("data.orderItems[].expectTime").type(JsonFieldType.ARRAY).optional()
+                            .description("주문 완료 시각"),
+                        fieldWithPath("data.orderItems[].totalPrice").type(JsonFieldType.NUMBER)
+                            .description("주문 총액")
+                    )
+                )
+            );
     }
 
+    @DisplayName("주문 수락 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void accept() throws Exception {
 
+        AcceptOrderRequest request = AcceptOrderRequest.builder()
+            .orderId(1L)
+            .prepareTime(30)
+            .build();
+
+        given(saleService.acceptOrder(eq(request.toAcceptOrderDto()), anyString()))
+            .willReturn(1L);
+
+        mockMvc.perform(
+            post("/sale/accept")
+                .content(objectMapper.writeValueAsString(request))
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("accept-order",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    requestFields(
+                        fieldWithPath("orderId").type(JsonFieldType.NUMBER)
+                            .description("주문 ID"),
+                        fieldWithPath("prepareTime").type(JsonFieldType.NUMBER)
+                            .description("주문 소요 시간")
+                    ),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NUMBER)
+                            .description("수락한 주문 ID")
+                    )
+                )
+            );
     }
 
+    @DisplayName("주문 거절 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void decline() throws Exception {
+        DeclineOrderRequest request = DeclineOrderRequest.builder()
+            .orderId(2L)
+            .reason("개인 사정")
+            .build();
 
+        given(saleService.declineOrder(eq(request.toDeclineOrderDto()), anyString()))
+            .willReturn(2L);
+
+        mockMvc.perform(
+                post("/sale/decline")
+                    .content(objectMapper.writeValueAsString(request))
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("decline-order",
+                    preprocessRequest(prettyPrint()),
+                    preprocessResponse(prettyPrint()),
+                    requestFields(
+                        fieldWithPath("orderId").type(JsonFieldType.NUMBER)
+                            .description("주문 ID"),
+                        fieldWithPath("reason").type(JsonFieldType.STRING)
+                            .description("주문 거절 사유")
+                    ),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NUMBER)
+                            .description("거절한 주문 ID")
+                    )
+                )
+            );
     }
 
+    @DisplayName("주문 일시 정지 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void pause() throws Exception {
+        given(saleService.pauseOrder(anyLong(), anyString()))
+            .willReturn(1L);
 
+        mockMvc.perform(
+                put("/sale/pause/1")
+                    .header("Authentication", "authentication")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("pause-order",
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NUMBER)
+                            .description("주문 일시 정지한 푸드트럭 ID")
+                    )
+                )
+            );
     }
 
+    @DisplayName("품절 메뉴 등록 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void soldout() throws Exception {
+        given(saleService.soldOutMenu(anyLong(), anyString()))
+            .willReturn(1L);
 
+        mockMvc.perform(
+                put("/sale/soldout/1")
+                    .header("Authentication", "authentication")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("soldout-menu",
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.NUMBER)
+                            .description("품절 등록한 메뉴 ID")
+                    )
+                )
+            );
     }
 
+    @DisplayName("영업 종료 API")
+    @Test
+    @WithMockUser(roles = "VENDOR")
     void closeSale() throws Exception {
 
+        CloseSaleResponse response = CloseSaleResponse.builder()
+            .saleId(1L)
+            .orderNumber(100)
+            .totalAmount(100000)
+            .createdTime(LocalDateTime.of(2023, 11, 1, 15, 0))
+            .endTime(LocalDateTime.of(2023, 11, 1, 20, 0))
+            .build();
+
+        given(saleService.closeSale(anyLong(), anyString()))
+            .willReturn(response);
+
+        mockMvc.perform(
+                put("/sale/close/1")
+                    .header("Authentication", "authentication")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andDo(
+                document("close-sale",
+                    preprocessResponse(prettyPrint()),
+                    responseFields(
+                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                            .description("코드"),
+                        fieldWithPath("status").type(JsonFieldType.STRING)
+                            .description("상태"),
+                        fieldWithPath("message").type(JsonFieldType.STRING)
+                            .description("메시지"),
+                        fieldWithPath("data").type(JsonFieldType.OBJECT)
+                            .description("종료한 영업 정보"),
+                        fieldWithPath("data.saleId").type(JsonFieldType.NUMBER)
+                            .description("종료한 영업 ID"),
+                        fieldWithPath("data.orderNumber").type(JsonFieldType.NUMBER)
+                            .description("영업 총 주문 수"),
+                        fieldWithPath("data.totalAmount").type(JsonFieldType.NUMBER)
+                            .description("영업 총 매출액"),
+                        fieldWithPath("data.createdTime").type(JsonFieldType.ARRAY)
+                            .description("영업 개시 시각"),
+                        fieldWithPath("data.endTime").type(JsonFieldType.ARRAY)
+                            .description("영업 종료 시각")
+                    )
+                )
+            );
     }
 }

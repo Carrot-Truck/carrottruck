d2002e50656994b9996fa2e56975eabe8c2f1492
qhfhdls12 <qhfhdls12@naver.com>
2023-11-02 10:08:03 +0900

    feat: 장바구니 조회 샘플코드 작성
    - Cart : Controller, Service, dto 변경

--- Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
@@ -1,5 +1,6 @@
 package com.boyworld.carrot.api.service.cart;
 
+import com.boyworld.carrot.api.controller.cart.response.CartResponse;
 import com.boyworld.carrot.api.service.cart.dto.CreateCartMenuDto;
 import com.boyworld.carrot.domain.cart.repository.RedisCartRepository;
 import jakarta.transaction.Transactional;
@@ -17,4 +18,9 @@ public class CartService {
     public Long createCart(CreateCartMenuDto createCartMenuDto, String email) {
         return null;
     }
+
+    public CartResponse getCart(String email) {
+        return null;
+    }
+
 }

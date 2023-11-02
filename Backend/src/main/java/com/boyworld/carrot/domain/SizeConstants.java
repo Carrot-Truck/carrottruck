6d8e7131927c11627db6320e0f9438c68d3c7edb
최영환 <longbright0804@gmail.com>
2023-11-02 16:59:20 +0900

    feat: 푸드트럭 위치정보 조회 쿼리 구현 - ST_DISTANCE 함수 사용 - MySQL Dialect 에서만 동작
    Closes #S09P31C211-210

--- Backend/src/main/java/com/boyworld/carrot/domain/SizeConstants.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/SizeConstants.java
@@ -1,6 +1,10 @@
 package com.boyworld.carrot.domain;
 
+import java.math.BigDecimal;
+
 public class SizeConstants {
 
     public static int PAGE_SIZE = 10;
+
+    public static BigDecimal SEARCH_RANGE_METER = BigDecimal.valueOf(1000);
 }
--- Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/ScheduleQueryRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/ScheduleQueryRepository.java
@@ -1,16 +1,104 @@
 package com.boyworld.carrot.domain.foodtruck.repository.query;
 
+import com.boyworld.carrot.api.service.foodtruck.dto.FoodTruckMarkerItem;
+import com.boyworld.carrot.domain.foodtruck.repository.dto.SearchCondition;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.boyworld.carrot.domain.SizeConstants.PAGE_SIZE;
+import static com.boyworld.carrot.domain.SizeConstants.SEARCH_RANGE_METER;
+import static com.boyworld.carrot.domain.foodtruck.QFoodTruck.foodTruck;
+import static com.boyworld.carrot.domain.foodtruck.QSchedule.schedule;
+import static com.boyworld.carrot.domain.sale.QSale.sale;
+import static com.querydsl.core.types.dsl.Expressions.numberTemplate;
+import static org.springframework.util.StringUtils.hasText;
 
 /**
  * 푸드트럭 스케줄 조회 레포지토리
  *
  * @author 최영환
  */
+@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class ScheduleQueryRepository {
     private final JPAQueryFactory queryFactory;
+
+    /**
+     * 푸드트럭 위치정보 조회 쿼리
+     *
+     * @param condition 검색 조건
+     * @return 현재 위치 기준 1Km 이내의 푸드트럭 마커 목록
+     */
+    public List<FoodTruckMarkerItem> getPositionsByCondition(SearchCondition condition) {
+        List<Long> ids = queryFactory
+                .select(schedule.id)
+                .from(schedule)
+                .join(schedule.foodTruck, foodTruck)
+                .join(sale).on(schedule.foodTruck.id.eq(sale.foodTruck.id))
+                .where(
+                        isEqualCategoryId(condition.getCategoryId()),
+                        nameLikeKeyword(condition.getKeyword()),
+                        isNearBy(condition, schedule.latitude, schedule.longitude),
+                        isActiveSchedule()
+                )
+                .limit(PAGE_SIZE + 1)
+                .fetch();
+
+        if (ids == null || ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+                .select(Projections.constructor(FoodTruckMarkerItem.class,
+                        schedule.foodTruck.category.id,
+                        schedule.foodTruck.id,
+                        calculateDistance(condition.getLatitude(), schedule.latitude,
+                                condition.getLongitude(), schedule.longitude),
+                        schedule.latitude,
+                        schedule.longitude,
+                        sale.endTime.isNull()
+                ))
+                .from(schedule)
+                .join(schedule.foodTruck, foodTruck)
+                .join(sale).on(schedule.foodTruck.id.eq(sale.foodTruck.id))
+                .where(
+                        schedule.id.in(ids)
+                )
+                .fetch();
+    }
+
+    private BooleanExpression isEqualCategoryId(Long categoryId) {
+        return categoryId != null ? foodTruck.category.id.eq(categoryId) : null;
+    }
+
+    private BooleanExpression nameLikeKeyword(String keyword) {
+        return hasText(keyword) ? foodTruck.name.contains(keyword) : null;
+    }
+
+    private BooleanExpression isNearBy(SearchCondition condition,
+                                       NumberPath<BigDecimal> targetLat, NumberPath<BigDecimal> targetLng) {
+        NumberTemplate<BigDecimal> distance = calculateDistance(condition.getLatitude(), targetLat,
+                condition.getLongitude(), targetLng);
+        return distance.loe(SEARCH_RANGE_METER);
+    }
+
+    private NumberTemplate<BigDecimal> calculateDistance(BigDecimal currentLat, NumberPath<BigDecimal> targetLat,
+                                                         BigDecimal currentLng, NumberPath<BigDecimal> targetLng) {
+        return Expressions.numberTemplate(BigDecimal.class,
+                "ST_DISTANCE(POINT({0}, {1}), POINT({2}, {3}))",
+                currentLat, currentLng, targetLat, targetLng);
+    }
+
+    private BooleanExpression isActiveSchedule() {
+        return schedule.active.isTrue();
+    }
 }

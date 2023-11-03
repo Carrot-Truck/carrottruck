33a27b2d6530086efdb0290354b0949031f736d7
최영환 <longbright0804@gmail.com>
2023-11-03 17:28:33 +0900

    feat: 푸드트럭 전체 목록 조회 쿼리 구현 - ScheduleQueryRepository#getFoodTrucksByCondition
    Closes #S09P31C211-210

--- Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/ScheduleQueryRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/foodtruck/repository/query/ScheduleQueryRepository.java
@@ -1,7 +1,11 @@
 package com.boyworld.carrot.domain.foodtruck.repository.query;
 
+import com.boyworld.carrot.api.controller.foodtruck.response.FoodTruckItem;
 import com.boyworld.carrot.api.service.foodtruck.dto.FoodTruckMarkerItem;
+import com.boyworld.carrot.domain.foodtruck.repository.dto.OrderCondition;
 import com.boyworld.carrot.domain.foodtruck.repository.dto.SearchCondition;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.*;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -15,10 +19,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.boyworld.carrot.domain.SizeConstants.PAGE_SIZE;
 import static com.boyworld.carrot.domain.SizeConstants.SEARCH_RANGE_METER;
 import static com.boyworld.carrot.domain.foodtruck.QFoodTruck.foodTruck;
+import static com.boyworld.carrot.domain.foodtruck.QFoodTruckImage.foodTruckImage;
+import static com.boyworld.carrot.domain.foodtruck.QFoodTruckLike.foodTruckLike;
 import static com.boyworld.carrot.domain.foodtruck.QSchedule.schedule;
+import static com.boyworld.carrot.domain.review.QReview.review;
 import static com.boyworld.carrot.domain.sale.QSale.sale;
+import static com.querydsl.jpa.JPAExpressions.select;
 import static org.springframework.util.StringUtils.hasText;
 
 /**
@@ -43,7 +52,7 @@ public class ScheduleQueryRepository {
                 .select(schedule.id)
                 .from(schedule)
                 .join(schedule.foodTruck, foodTruck)
-                .join(sale).on(schedule.foodTruck.id.eq(sale.foodTruck.id))
+                .leftJoin(sale).on(schedule.foodTruck.eq(sale.foodTruck)).fetchJoin()
                 .where(
                         isEqualCategoryId(condition.getCategoryId()),
                         nameLikeKeyword(condition.getKeyword()),
@@ -58,6 +67,7 @@ public class ScheduleQueryRepository {
 
         LocalDateTime today = LocalDate.now().atStartOfDay();
         LocalDateTime now = LocalDateTime.now();
+
         return queryFactory
                 .select(Projections.constructor(FoodTruckMarkerItem.class,
                         schedule.foodTruck.category.id,
@@ -70,11 +80,106 @@ public class ScheduleQueryRepository {
                 ))
                 .from(schedule)
                 .join(schedule.foodTruck, foodTruck)
-                .join(sale).on(schedule.foodTruck.id.eq(sale.foodTruck.id))
+                .leftJoin(sale).on(schedule.foodTruck.eq(sale.foodTruck)).fetchJoin()
                 .where(
                         schedule.id.in(ids)
                 )
                 .fetch();
+    }
+
+    /**
+     * 근처 푸드트럭 목록 조회 API
+     *
+     * @param condition 검색 조건
+     * @return 현재 위치 기반 반경 1Km 이내의 푸드트럭 목록
+     */
+    public List<FoodTruckItem> getFoodTrucksByCondition(SearchCondition condition) {
+        List<Long> ids = queryFactory
+                .select(schedule.id)
+                .from(schedule)
+                .join(schedule.foodTruck, foodTruck)
+                .leftJoin(sale).on(schedule.foodTruck.eq(sale.foodTruck)).fetchJoin()
+                .leftJoin(foodTruckLike).on(schedule.foodTruck.eq(foodTruckLike.foodTruck)).fetchJoin()
+                .leftJoin(review).on(schedule.foodTruck.eq(review.foodTruck)).fetchJoin()
+                .leftJoin(foodTruckImage).on(schedule.foodTruck.eq(foodTruckImage.foodTruck)).fetchJoin()
+                .where(
+                        isEqualCategoryId(condition.getCategoryId()),
+                        nameLikeKeyword(condition.getKeyword()),
+                        isNearBy(condition, schedule.latitude, schedule.longitude),
+                        isActive(),
+                        isActiveSchedule()
+                )
+                .fetch();
+
+        if (ids == null || ids.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime today = LocalDate.now().atStartOfDay();
+        LocalDateTime lastMonth = now.minusMonths(1);
+
+        NumberTemplate<BigDecimal> distance = calculateDistance(condition.getLatitude(), schedule.latitude,
+                condition.getLongitude(), schedule.longitude);
+
+        // TODO: 2023-11-03 리팩토링
+        
+        return queryFactory
+                .select(Projections.constructor(FoodTruckItem.class,
+                        schedule.foodTruck.category.id,
+                        schedule.foodTruck.id,
+                        schedule.foodTruck.name,
+                        isOpen(today, now),
+                        foodTruckLike.isNotNull().and(foodTruckLike.active.isTrue()),
+                        schedule.foodTruck.prepareTime,
+                        select(foodTruckLike.count().intValue()).from(foodTruckLike).where(foodTruckLike.foodTruck.eq(schedule.foodTruck)),
+                        select(review.grade.sum().divide(review.count()).doubleValue()).from(review).where(review.foodTruck.eq(schedule.foodTruck)),
+                        select(review.count().intValue()).from(review).where(review.foodTruck.eq(schedule.foodTruck)),
+                        distance,
+                        schedule.address,
+                        foodTruckImage.uploadFile.storeFileName,
+                        new CaseBuilder()
+                                .when(foodTruck.createdDate.after(lastMonth))
+                                .then(true)
+                                .otherwise(false)
+                ))
+                .from(schedule)
+                .join(schedule.foodTruck, foodTruck)
+                .leftJoin(sale).on(schedule.foodTruck.eq(sale.foodTruck)).fetchJoin()
+                .leftJoin(foodTruckLike).on(schedule.foodTruck.eq(foodTruckLike.foodTruck)).fetchJoin()
+                .leftJoin(review).on(schedule.foodTruck.eq(review.foodTruck)).fetchJoin()
+                .leftJoin(foodTruckImage).on(schedule.foodTruck.eq(foodTruckImage.foodTruck)).fetchJoin()
+                .where(
+                        schedule.id.in(ids)
+                )
+                .orderBy(
+                        createOrderSpecifiers(condition.getOrderCondition(), distance)
+                )
+                .limit(PAGE_SIZE + 1)
+                .fetch();
+    }
+
+    private BooleanExpression isActive() {
+        return foodTruck.active;
+    }
+
+    private OrderSpecifier<?>[] createOrderSpecifiers(OrderCondition orderCondition, NumberTemplate<BigDecimal> distance) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+        orderSpecifiers.add(new OrderSpecifier<>(Order.ASC, distance));
+
+        if (orderCondition == null) {
+            return orderSpecifiers.toArray(new OrderSpecifier[0]);
+        }
+
+        if (orderCondition.equals(OrderCondition.LIKE)) {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, foodTruckLike.count()));
+        } else if (orderCondition.equals(OrderCondition.GRADE)) {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, review.grade.avg()));
+        } else if (orderCondition.equals(OrderCondition.REVIEW)) {
+            orderSpecifiers.add(new OrderSpecifier<>(Order.DESC, review.count()));
+        }
+
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
     }
 
     private BooleanExpression isEqualCategoryId(Long categoryId) {
@@ -122,6 +227,6 @@ public class ScheduleQueryRepository {
     }
 
     private BooleanExpression isActiveSchedule() {
-        return schedule.active.isTrue();
+        return schedule.active;
     }
 }

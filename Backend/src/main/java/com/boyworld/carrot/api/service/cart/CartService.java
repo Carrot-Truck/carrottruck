c98fa678a7cecd3f5e954ff88ffebdabae328028
김동현 <qhfhdls12@naver.com>
2023-11-06 07:01:26 +0900

    feat: cartService Repo 주석 제거
    - CartService RedisCartRepository 주석 제거

--- Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/cart/CartService.java
@@ -3,7 +3,8 @@ package com.boyworld.carrot.api.service.cart;
 import com.boyworld.carrot.api.controller.cart.response.CartOrderResponse;
 import com.boyworld.carrot.api.controller.cart.response.CartResponse;
 import com.boyworld.carrot.api.service.cart.dto.CreateCartMenuDto;
-//import com.boyworld.carrot.domain.cart.repository.RedisCartRepository;
+import com.boyworld.carrot.domain.cart.Cart;
+import com.boyworld.carrot.domain.cart.repository.RedisCartRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,10 +15,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Transactional
 public class CartService {
-//    private final RedisCartRepository redisCartRepository;
+    private final RedisCartRepository redisCartRepository;
 
     public Long createCart(CreateCartMenuDto createCartMenuDto, String email) {
+        // 회원의 카트가 존재하는지?
+        // 회원의 카트가 존재하면 같은 푸드트럭의 카트가 존재하는 지?
+        // 카트 및 카트 메뉴 추가
+        Cart cart = Cart.builder()
+                .id(email)
+                .foodTruckId(createCartMenuDto.getFoodTruckId())
+                .build();
+
+        redisCartRepository.save(cart);
         return null;
+
     }
 
     public CartResponse getCart(String email) {
--- Backend/src/main/java/com/boyworld/carrot/domain/cart/repository/RedisCartRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/cart/repository/RedisCartRepository.java
@@ -1,8 +1,8 @@
-//package com.boyworld.carrot.domain.cart.repository;
-//
-//import com.boyworld.carrot.domain.cart.Cart;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface RedisCartRepository extends CrudRepository<Cart, Long> {
-//
-//}
+package com.boyworld.carrot.domain.cart.repository;
+
+import com.boyworld.carrot.domain.cart.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RedisCartRepository extends CrudRepository<Cart, Long> {
+
+}

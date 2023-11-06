9a393e7298c76c9ff0f525a5e33ef4fe67e6851b
longBright <longbright0804@gmail.com>
2023-11-06 21:40:26 +0900

    refactor: 메뉴 조회 서비스 불필요 변수 제거
    Closes #S09P31C211-231

--- Backend/src/main/java/com/boyworld/carrot/api/service/menu/MenuQueryService.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/menu/MenuQueryService.java
@@ -1,15 +1,9 @@
 package com.boyworld.carrot.api.service.menu;
 
+import com.boyworld.carrot.api.controller.menu.response.MenuDetailResponse;
 import com.boyworld.carrot.api.controller.menu.response.MenuOptionResponse;
 import com.boyworld.carrot.api.controller.menu.response.MenuResponse;
-import com.boyworld.carrot.api.controller.menu.response.MenuDetailResponse;
-import com.boyworld.carrot.api.service.member.error.InValidAccessException;
 import com.boyworld.carrot.api.service.menu.dto.MenuDto;
-import com.boyworld.carrot.domain.foodtruck.FoodTruck;
-import com.boyworld.carrot.domain.foodtruck.repository.command.FoodTruckRepository;
-import com.boyworld.carrot.domain.member.Member;
-import com.boyworld.carrot.domain.member.Role;
-import com.boyworld.carrot.domain.member.repository.command.MemberRepository;
 import com.boyworld.carrot.domain.menu.repository.query.MenuOptionQueryRepository;
 import com.boyworld.carrot.domain.menu.repository.query.MenuQueryRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +12,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 /**
  * 메뉴 조회 서비스
@@ -32,8 +25,6 @@ import java.util.NoSuchElementException;
 public class MenuQueryService {
 
     private final MenuQueryRepository menuQueryRepository;
-    private final MemberRepository memberRepository;
-    private final FoodTruckRepository foodTruckRepository;
     private final MenuOptionQueryRepository menuOptionQueryRepository;
 
     /**
@@ -58,63 +49,5 @@ public class MenuQueryService {
         List<MenuOptionResponse> menuOptions = menuOptionQueryRepository.getMenuOptionsByMenuId(menuId);
 
         return MenuDetailResponse.of(menu, menuOptions);
-    }
-
-    /**
-     * 이메일로 회원 엔티티 조회
-     *
-     * @param email 현재 로그인한 사용자 이메일
-     * @return 이메일에 해당하는 회원 엔티티
-     */
-    private Member getMemberByEmail(String email) {
-        return memberRepository.findByEmail(email)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 회원입니다."));
-    }
-
-    /**
-     * 접근 유효성 판별
-     *
-     * @param member 회원 엔티티
-     * @throws InValidAccessException 해당 회원의 권한이 CLIENT 이거나 비활성화 상태인 경우
-     */
-    private void checkValidMemberAccess(Member member) {
-        if (isClient(member.getRole()) || !member.getActive()) {
-            throw new InValidAccessException("잘못된 접근입니다.");
-        }
-    }
-
-    /**
-     * 푸드트럭 식별키로 푸드트럭 조회
-     *
-     * @param foodTruckId 푸드트럭 식별키
-     * @return 푸드트럭 엔티티
-     * @throws NoSuchElementException 식별키에 해당하는 푸드트럭이 없는 경우
-     */
-    private FoodTruck getFoodTruckById(Long foodTruckId) {
-        return foodTruckRepository.findById(foodTruckId)
-                .orElseThrow(() -> new NoSuchElementException("존재하지 않는 푸드트럭입니다."));
-    }
-
-    /**
-     * 소유주 접근 여부 판별
-     *
-     * @param member    접근하려는 회원
-     * @param foodTruck 접근하려는 푸드트럭
-     * @throws InValidAccessException 해당 회원이 해당 푸드트럭의 소유주가 아닌 경우
-     */
-    private void checkOwnerAccess(Member member, FoodTruck foodTruck) {
-        if (!foodTruck.getVendor().getId().equals(member.getId())) {
-            throw new InValidAccessException("잘못된 접근입니다.");
-        }
-    }
-
-    /**
-     * 회원 권한이 CLIENT 인지 여부 판별
-     *
-     * @param role 회원 권한
-     * @return true : CLIENT 인 경우 false: 그 외 경우
-     */
-    private boolean isClient(Role role) {
-        return role.equals(Role.CLIENT);
     }
 }

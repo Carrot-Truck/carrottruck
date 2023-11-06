346d2ef5916a032186d86ed0d8c22cfa82cedf0d
longBright <longbright0804@gmail.com>
2023-11-07 01:01:28 +0900

    test: 메뉴 수정 서비스 테스트 작성
    Closes #S09P31C211-233

--- Backend/src/test/java/com/boyworld/carrot/api/service/menu/MenuServiceTest.java
+++ Backend/src/test/java/com/boyworld/carrot/api/service/menu/MenuServiceTest.java
@@ -2,8 +2,10 @@ package com.boyworld.carrot.api.service.menu;
 
 import com.boyworld.carrot.IntegrationTestSupport;
 import com.boyworld.carrot.api.controller.menu.response.CreateMenuResponse;
+import com.boyworld.carrot.api.service.member.error.InValidAccessException;
 import com.boyworld.carrot.api.service.menu.dto.CreateMenuDto;
 import com.boyworld.carrot.api.service.menu.dto.CreateMenuOptionDto;
+import com.boyworld.carrot.api.service.menu.dto.EditMenuDto;
 import com.boyworld.carrot.domain.foodtruck.Category;
 import com.boyworld.carrot.domain.foodtruck.FoodTruck;
 import com.boyworld.carrot.domain.foodtruck.repository.command.CategoryRepository;
@@ -11,6 +13,8 @@ import com.boyworld.carrot.domain.foodtruck.repository.command.FoodTruckReposito
 import com.boyworld.carrot.domain.member.Member;
 import com.boyworld.carrot.domain.member.Role;
 import com.boyworld.carrot.domain.member.repository.command.MemberRepository;
+import com.boyworld.carrot.domain.menu.Menu;
+import com.boyworld.carrot.domain.menu.MenuInfo;
 import com.boyworld.carrot.domain.menu.repository.command.MenuImageRepository;
 import com.boyworld.carrot.domain.menu.repository.command.MenuOptionRepository;
 import com.boyworld.carrot.domain.menu.repository.command.MenuRepository;
@@ -24,6 +28,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class MenuServiceTest extends IntegrationTestSupport {
 
@@ -101,6 +106,150 @@ class MenuServiceTest extends IntegrationTestSupport {
                         "description", "menuOptionSize")
                 .containsExactlyInAnyOrder(dto.getMenuName(), foodTruck.getId(), dto.getPrice(),
                         dto.getDescription(), dto.getMenuOptionDtos().size());
+    }
+
+    @DisplayName("일반 사용자이거나 본인 소유의 푸드트럭이 아닐 경우 예외가 발생한다.")
+    @Test
+    void createMenuInValidAccess() {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+        Member vendor2 = createMember(Role.VENDOR, "hi@ssafy.com");
+        Member client1 = createMember(Role.CLIENT, "ssafy123@ssafy.com");
+        Member client2 = createMember(Role.CLIENT, "hello123@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        CreateMenuOptionDto option1 = CreateMenuOptionDto.builder()
+                .menuOptionName("옵션1")
+                .menuOptionPrice(500)
+                .menuOptionDescription("설명1")
+                .build();
+
+        CreateMenuOptionDto option2 = CreateMenuOptionDto.builder()
+                .menuOptionName("옵션2")
+                .menuOptionPrice(300)
+                .menuOptionDescription("설명2")
+                .build();
+
+        CreateMenuDto dto = CreateMenuDto.builder()
+                .foodTruckId(foodTruck.getId())
+                .menuName("달콤짭짤한 밥도둑 된장 삼겹살 구이")
+                .price(8900)
+                .description("동현 된장삼겹의 시그니쳐. 오직 된장 삼겹살 구이만!")
+                .menuOptionDtos(List.of(option1, option2))
+                .build();
+
+        String path = "menu_test.png";
+        String contentType = "image/png";
+        MockMultipartFile file = new MockMultipartFile("menu_test", path, contentType, "menu_test".getBytes());
+
+        // when // then
+        assertThatThrownBy(() -> menuService.createMenu(dto, file, vendor2.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
+
+        assertThatThrownBy(() -> menuService.createMenu(dto, file, client1.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
+    }
+
+    @DisplayName("사업자는 본인 소유의 푸드트럭의 메뉴와 이미지를 수정할 수 있다.")
+    @Test
+    void editMenuWithMenuImageAndOptions() throws IOException {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+        Member vendor2 = createMember(Role.VENDOR, "hi@ssafy.com");
+        Member client1 = createMember(Role.CLIENT, "ssafy123@ssafy.com");
+        Member client2 = createMember(Role.CLIENT, "hello123@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        Menu menu = Menu.builder()
+                .foodTruck(foodTruck)
+                .menuInfo(MenuInfo.builder().name("메뉴1").price(10000).description("메뉴 설명1").soldOut(false).build())
+                .active(true)
+                .build();
+        menu = menuRepository.save(menu);
+
+        EditMenuDto dto = EditMenuDto.builder()
+                .menuId(menu.getId())
+                .menuName("짭짤한 밥도둑 삼겹살 구이")
+                .menuPrice(9900)
+                .menuDescription("동현 삼겹의 시그니쳐. 오직 된장 삼겹살 구이만!")
+                .build();
+
+        String path = "menu_edit_test.png";
+        String contentType = "image/png";
+        MockMultipartFile file = new MockMultipartFile("menu_edit_test", path, contentType, "menu_edit_test".getBytes());
+
+        // when
+        Long menuId = menuService.editMenu(dto, file, vendor1.getEmail());
+        Menu editedMenu = menuRepository.findById(menuId)
+                .orElseThrow();
+
+        // then
+        assertThat(editedMenu.getMenuInfo()).extracting("name", "description", "price", "soldOut")
+                .containsExactly(dto.getMenuName(), dto.getMenuDescription(), dto.getMenuPrice(), false);
+    }
+
+    @DisplayName("일반 사용자이거나 본인 소유의 푸드트럭이 아닌 경우 예외가 발생한다.")
+    @Test
+    void editMenuInValidAccess() throws IOException {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+        Member vendor2 = createMember(Role.VENDOR, "hi@ssafy.com");
+        Member client1 = createMember(Role.CLIENT, "ssafy123@ssafy.com");
+        Member client2 = createMember(Role.CLIENT, "hello123@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        Menu menu = Menu.builder()
+                .foodTruck(foodTruck)
+                .menuInfo(MenuInfo.builder().name("메뉴1").price(10000).description("메뉴 설명1").soldOut(false).build())
+                .active(true)
+                .build();
+        menu = menuRepository.save(menu);
+
+        EditMenuDto dto = EditMenuDto.builder()
+                .menuId(menu.getId())
+                .menuName("짭짤한 밥도둑 삼겹살 구이")
+                .menuPrice(9900)
+                .menuDescription("동현 삼겹의 시그니쳐. 오직 된장 삼겹살 구이만!")
+                .build();
+
+        String path = "menu_edit_test.png";
+        String contentType = "image/png";
+        MockMultipartFile file = new MockMultipartFile("menu_edit_test", path, contentType, "menu_edit_test".getBytes());
+
+        // when // then
+        assertThatThrownBy(() -> menuService.editMenu(dto, file, vendor2.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
+
+        assertThatThrownBy(() -> menuService.editMenu(dto, file, client1.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
     }
 
     private Member createMember(Role role, String email) {

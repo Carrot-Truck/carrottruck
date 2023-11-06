05483c49a8af1548cc492943b98ade4bad604924
양진형 <jinhyugn@gmail.com>
2023-11-06 21:55:35 +0900

    fix: SalesStatisticsDto 이름 변경

--- Backend/src/main/java/com/boyworld/carrot/api/service/sale/dto/SalesStatisticsDto.java
+++ Backend/src/main/java/com/boyworld/carrot/api/service/sale/dto/SalesStatisticsDto.java
@@ -3,11 +3,10 @@ package com.boyworld.carrot.api.service.sale.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-public class SalesDto {
+public class SalesStatisticsDto {
 
     private Long salesId;
 
@@ -20,8 +19,8 @@ public class SalesDto {
     private Integer totalAmount;
 
     @Builder
-    public SalesDto(Long salesId, LocalDateTime startTime, LocalDateTime endTime,
-                    String address, Integer totalAmount) {
+    public SalesStatisticsDto(Long salesId, LocalDateTime startTime, LocalDateTime endTime,
+                              String address, Integer totalAmount) {
         this.salesId = salesId;
         this.startTime = startTime;
         this.endTime = endTime;

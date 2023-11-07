560af948a965eaca3fbcd59970b636d010fd0a34
최영환 <longbright0804@gmail.com>
2023-11-07 14:18:00 +0900

    test: 푸드트럭 스케줄 상세조회 서비스 테스트 작성
    Closes #S09P31C211-238

--- Backend/src/test/java/com/boyworld/carrot/api/service/schedule/ScheduleQueryServiceTest.java
+++ Backend/src/test/java/com/boyworld/carrot/api/service/schedule/ScheduleQueryServiceTest.java
@@ -1,7 +1,9 @@
 package com.boyworld.carrot.api.service.schedule;
 
 import com.boyworld.carrot.IntegrationTestSupport;
+import com.boyworld.carrot.api.controller.schedule.response.ScheduleDetailResponse;
 import com.boyworld.carrot.api.controller.schedule.response.ScheduleResponse;
+import com.boyworld.carrot.api.service.member.error.InValidAccessException;
 import com.boyworld.carrot.api.service.schedule.dto.ScheduleDto;
 import com.boyworld.carrot.domain.foodtruck.Category;
 import com.boyworld.carrot.domain.foodtruck.FoodTruck;
@@ -21,9 +23,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * 스케줄 조회 서비스 테스트
@@ -53,7 +58,7 @@ class ScheduleQueryServiceTest extends IntegrationTestSupport {
 
     @DisplayName("사용자는 푸드트럭 식별키로 해당 푸드트럭의 스케줄 리스트를 조회할 수 있다.")
     @Test
-    void getSchedulesByFoodTruckId() {
+    void getSchedules() {
         // given
         Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
 
@@ -76,6 +81,118 @@ class ScheduleQueryServiceTest extends IntegrationTestSupport {
 
         // then
         assertThat(response.getSchedules()).hasSize(3);
+    }
+
+    @DisplayName("사업자는 푸드트럭 스케줄 식별키로 보유한 푸드트럭의 스케줄의 상세 정보를 조회할 수 있다.")
+    @Test
+    void getScheduleAsOwner() {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        Schedule schedule = createSchedule(foodTruck,
+                BigDecimal.valueOf(37.5665),
+                BigDecimal.valueOf(126.9780),
+                LocalDateTime.now().minusHours(1),
+                LocalDateTime.now().plusHours(5),
+                LocalDateTime.now().getDayOfWeek().name(),
+                foodTruck.getId() + " schedule1 address"
+        );
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+        // when
+        ScheduleDetailResponse response =
+                scheduleQueryService.getSchedule(savedSchedule.getId(), foodTruck.getId(), vendor1.getEmail());
+        log.debug("response={}", response);
+
+        // then
+        assertThat(response)
+                .extracting("address", "dayOfWeek", "latitude", "longitude", "startTime", "endTime")
+                .containsExactly(savedSchedule.getAddress(),
+                        savedSchedule.getDayOfWeek().name(),
+                        savedSchedule.getLatitude().toString(),
+                        savedSchedule.getLongitude().toString(),
+                        savedSchedule.getStartTime().format(DateTimeFormatter.ofPattern("HH:mm")),
+                        savedSchedule.getEndTime().format(DateTimeFormatter.ofPattern("HH:mm")));
+    }
+
+    @DisplayName("푸드트럭 스케줄 식별키에 해당하는 스케줄이 없으면 예외가 발생한다.")
+    @Test
+    void getScheduleIsNull() {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        Schedule schedule = createSchedule(foodTruck,
+                BigDecimal.valueOf(37.5665),
+                BigDecimal.valueOf(126.9780),
+                LocalDateTime.now().minusHours(1),
+                LocalDateTime.now().plusHours(5),
+                LocalDateTime.now().getDayOfWeek().name(),
+                foodTruck.getId() + " schedule1 address"
+        );
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+        // when // then
+        assertThatThrownBy(() -> scheduleQueryService.getSchedule(-1L, foodTruck.getId(), vendor1.getEmail()))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("존재하지 않는 스케줄입니다.");
+
+    }
+
+    @DisplayName("일반 사용자이거나 푸드트럭 소유주가 아니면 예외가 발생한다.")
+    @Test
+    void getScheduleAsInValidMember() {
+        // given
+        Member vendor1 = createMember(Role.VENDOR, "ssafy@ssafy.com");
+        Member vendor2 = createMember(Role.VENDOR, "hi@ssafy.com");
+        Member client1 = createMember(Role.CLIENT, "ssafy123@ssafy.com");
+
+        Category category = createCategory("고기/구이");
+
+        FoodTruck foodTruck = createFoodTruck(vendor1, category, "동현 된장삼겹", "010-1234-5678",
+                "돼지고기(국산), 고축가루(국산), 참깨(중국산), 양파(국산), 대파(국산), 버터(프랑스)",
+                "된장 삼겹 구이 & 삼겹 덮밥 전문 푸드트럭",
+                40,
+                10,
+                true);
+
+        Schedule schedule = createSchedule(foodTruck,
+                BigDecimal.valueOf(37.5665),
+                BigDecimal.valueOf(126.9780),
+                LocalDateTime.now().minusHours(1),
+                LocalDateTime.now().plusHours(5),
+                LocalDateTime.now().getDayOfWeek().name(),
+                foodTruck.getId() + " schedule1 address"
+        );
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+
+        // when // then
+        assertThatThrownBy(() ->
+                scheduleQueryService.getSchedule(savedSchedule.getId(), foodTruck.getId(), vendor2.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
+
+        assertThatThrownBy(() ->
+                scheduleQueryService.getSchedule(savedSchedule.getId(), foodTruck.getId(), client1.getEmail()))
+                .isInstanceOf(InValidAccessException.class)
+                .hasMessage("잘못된 접근입니다.");
     }
 
     private Member createMember(Role role, String email) {

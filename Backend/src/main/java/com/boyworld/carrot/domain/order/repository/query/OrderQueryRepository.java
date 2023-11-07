847d9f0c003048857795419d14680c11b696528e
박은규 <parkeg1223@gmail.com>
2023-11-07 17:07:59 +0900

    fix: 주문 상태 변경 로직 변경에 따른 메서드 삭제

--- Backend/src/main/java/com/boyworld/carrot/domain/order/repository/query/OrderQueryRepository.java
+++ Backend/src/main/java/com/boyworld/carrot/domain/order/repository/query/OrderQueryRepository.java
@@ -1,10 +1,6 @@
 package com.boyworld.carrot.domain.order.repository.query;
 
-import static com.boyworld.carrot.domain.order.QOrder.order;
-
-import com.boyworld.carrot.domain.order.Status;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
@@ -15,26 +11,4 @@ import org.springframework.stereotype.Repository;
 public class OrderQueryRepository {
 
     private final JPAQueryFactory queryFactory;
-
-    public Long updateOrderAccepted(Long orderId, Integer prepareTime) {
-        queryFactory
-            .update(order)
-            .set(order.status, Status.PROCESSING)
-            .set(order.expectTime, LocalDateTime.now().plusMinutes(prepareTime))
-            .where(order.id.eq(orderId))
-            .execute();
-
-        return orderId;
-    }
-
-    public Long updateOrderDeclined(Long orderId, String reason) {
-        queryFactory
-            .update(order)
-            .set(order.status, Status.DECLINED)
-            .where(order.id.eq(orderId))
-            .execute();
-
-        return orderId;
-    }
-
 }

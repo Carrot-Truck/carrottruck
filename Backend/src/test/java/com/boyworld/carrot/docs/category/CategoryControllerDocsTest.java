668d81364ea2fe1c5480122c636eba28c596adf6
최영환 <longbright0804@gmail.com>
2023-11-07 16:34:13 +0900

    feat: 카테고리 CUD 서비스 삭제 및 반영
    Closes #S09P31C211-245

--- Backend/src/test/java/com/boyworld/carrot/docs/category/CategoryControllerDocsTest.java
+++ Backend/src/test/java/com/boyworld/carrot/docs/category/CategoryControllerDocsTest.java
@@ -3,9 +3,6 @@ package com.boyworld.carrot.docs.category;
 import com.boyworld.carrot.api.controller.foodtruck.CategoryController;
 import com.boyworld.carrot.api.controller.foodtruck.response.*;
 import com.boyworld.carrot.api.service.foodtruck.CategoryQueryService;
-import com.boyworld.carrot.api.service.foodtruck.CategoryService;
-import com.boyworld.carrot.api.service.foodtruck.FoodTruckQueryService;
-import com.boyworld.carrot.api.service.foodtruck.FoodTruckService;
 import com.boyworld.carrot.docs.RestDocsSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,26 +12,22 @@ import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CategoryControllerDocsTest.class)
 public class CategoryControllerDocsTest extends RestDocsSupport {
-
-    private final CategoryService categoryService = mock(CategoryService.class);
     private final CategoryQueryService categoryQueryService = mock(CategoryQueryService.class);
 
     @Override
     protected Object initController() {
-        return new CategoryController(categoryService, categoryQueryService);
+        return new CategoryController(categoryQueryService);
     }
 
     @DisplayName("카테고리 목록 조회 API")

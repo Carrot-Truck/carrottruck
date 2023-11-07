7f7fc981e6ecee50fb4b0439b09339bf1ff9cafd
gunhoo <gunhoo2016@gmail.com>
2023-11-07 14:20:17 +0900

    feat: vendor main page (영업시작버튼, 푸드트럭등록버튼)

--- frontend/vendor/src/pages/MainPage/index.tsx
+++ frontend/vendor/src/pages/MainPage/index.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { MainPageLayout } from './style';
-import Button from '../../components/atoms/Button';
-import SearchBar from '../../components/atoms/SearchBar';
-import FoodTruckMenu from 'components/organisms/FoodTruckMenu';
-import NaverMap from 'components/atoms/Map';
+import { useEffect, useState } from 'react';
 import Navbar from 'components/organisms/Navbar';
+import RegistFoodTruckButton from 'components/organisms/RegistFoodTruckButton'
+import VendorMainForm from 'components/organisms/VendorMainForm'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function MainPage() {
-  const MapID = '0n8mf3dv6k';
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/login');
-  };
-  const [searchValue, setSearchValue] = useState('');
+   // 데이터가 비어있는지 여부를 확인할 상태 변수를 정의합니다.
+   const [dataEmpty, setDataEmpty] = useState(true); // 초기값은 true로 설정합니다.
+   const navigate = useNavigate();
+
+   useEffect (()=>{
+    const getDataL = async () => {
+      try {
+        const response = await axios.get('http://localhost:8001/food-truck/overview?lastFoodTruckId=');
+        if (response.data.success) {
+          setDataEmpty(response.data.items.length === 0);
+        }
+      } catch (error) {
+        console.error('Error!!', error);
+        alert("로그인이 필요합니다.");
+        navigate('/login');
+      }
+    };
+    getDataL();
+   }, []);
 
   return (
     <MainPageLayout>
-      <Button size="full" radius="m" color="Primary" text="어우 귀찮아..." handleClick={handleClick}></Button>
-      <SearchBar
-        size="full"
-        confirmSearch={() => {}}
-        value={searchValue}
-        setValue={setSearchValue}
-        color="Primary"
-        placeholder="검색어를 입력하세요"
-      />
-      <NaverMap clientId={MapID} markers={[{ latitude: 35.19398974745785, longitude: 126.813153026795 }]}></NaverMap>
-      <FoodTruckMenu></FoodTruckMenu>
+      {dataEmpty ? ( // 데이터가 비어 있으면 RegistFoodTruckButton을 표시합니다.
+        <RegistFoodTruckButton />
+      ) : ( // 데이터가 비어 있지 않으면 VendorForm을 표시합니다.
+        <VendorMainForm /> 
+      )}
       <Navbar></Navbar>
     </MainPageLayout>
   );

b43ad33493ef4678ec4c01296423237f75cbd315
양진형 <jinhyugn@naver.com>
2023-11-09 12:48:23 +0900

    feat: api 호출 코드 작성

---
+++ frontend/vendor/src/api/member/memberAddress.tsx
@@ -0,0 +1,27 @@
+import { memberAddressApi } from "../index";
+
+const api = memberAddressApi
+
+export async function createMemberAddress(data: Object, success: any, fail: any) {
+    await api.post(``, data).then(success).catch(fail);
+}
+
+export async function getMemberAddresses(data: Object, success: any, fail: any) {
+    await api.get(``, { params: data }).then(success).catch(fail);
+}
+
+export async function getMemberAddress(memberAddressId: number, success: any, fail: any) {
+    await api.get(`/${memberAddressId}}`).then(success).catch(fail);
+}
+
+export async function editMemberAddress(memberAddressId: number, data: Object, success: any, fail: any) {
+    await api.patch(`/${memberAddressId}}`, data).then(success).catch(fail);
+}
+
+export async function editSelected(data: Object, success: any, fail: any) {
+    await api.patch(`/selected`, data).then(success).catch(fail);
+}
+
+export async function deleteMemberAddress(memberAddressId: number, success: any, fail: any) {
+    await api.delete(`/${memberAddressId}`).then(success).catch(fail);
+}

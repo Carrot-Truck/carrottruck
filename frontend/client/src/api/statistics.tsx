6108ca58c9e1b36cc9961d593771667527f1cea1
박은규 <parkeg1223@gmail.com>
2023-11-10 21:01:46 +0900

    feat: client api 디렉토리 추가

---
+++ frontend/client/src/api/statistics.tsx
@@ -0,0 +1,27 @@
+import { statisticsApi } from "./index";
+
+const api = statisticsApi
+
+export async function getStatisticsBySales(foodTruckId: number, data: Object, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/sales`, { params: data }).then(success).catch(fail);
+}
+
+export async function getStatisticsBySalesDetails(foodTruckId: number, salesId: number, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/sales-detail/${salesId}`).then(success).catch(fail);
+}
+
+export async function getStatisticsByWeek(foodTruckId: number, data: Object, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/weekly`, { params: data }).then(success).catch(fail);
+}
+
+export async function getStatisticsByWeekDetails(foodTruckId: number, data: Object, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/weekly-detail`, { params: data }).then(success).catch(fail);
+}
+
+export async function getStatisticsByMonth(foodTruckId: number, data: Object, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/monthly`, { params: data }).then(success).catch(fail);
+}
+
+export async function getStatisticsByMonthDetails(foodTruckId: number, data: Object, success: any, fail: any) {
+    await api.get(`/${foodTruckId}/monthly-detail`, { params: data }).then(success).catch(fail);
+}

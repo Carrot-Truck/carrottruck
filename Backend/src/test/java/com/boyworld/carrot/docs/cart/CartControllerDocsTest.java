9b7aac42905afd8916c540f78d8760808363d05b
qhfhdls12 <qhfhdls12@naver.com>
2023-11-10 11:25:03 +0900

    test: CartDocsTest 에러 수정
    - CartDocsTest 에러 수정

--- Backend/src/test/java/com/boyworld/carrot/docs/cart/CartControllerDocsTest.java
+++ Backend/src/test/java/com/boyworld/carrot/docs/cart/CartControllerDocsTest.java
@@ -2,9 +2,15 @@ package com.boyworld.carrot.docs.cart;
 
 import com.boyworld.carrot.api.controller.cart.CartController;
 import com.boyworld.carrot.api.controller.cart.request.CreateCartMenuRequest;
+import com.boyworld.carrot.api.controller.cart.response.CartResponse;
 import com.boyworld.carrot.api.service.cart.CartService;
+import com.boyworld.carrot.api.service.cart.dto.CartMenuDto;
+import com.boyworld.carrot.api.service.cart.dto.CartMenuOptionDto;
 import com.boyworld.carrot.api.service.cart.dto.CreateCartMenuDto;
 import com.boyworld.carrot.docs.RestDocsSupport;
+import com.boyworld.carrot.domain.cart.Cart;
+import com.boyworld.carrot.domain.cart.CartMenu;
+import com.boyworld.carrot.domain.cart.CartMenuOption;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,15 +20,16 @@ import org.springframework.security.test.context.support.WithMockUser;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;

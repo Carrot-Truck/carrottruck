2f1851e9d64aca28532f687e40defe3e13651912
이정찬 <wjdcks1203@naver.com>
2023-11-11 17:04:57 +0900

    feat: 메뉴 추가 화면

---
+++ frontend/vendor/src/pages/FoodMenuDetailModifyPage/style.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const FoodMenuDetailModifyLayout = styled.div`
+  width: 360px;
+  padding-bottom: 200px;
+  font-family: BMJUA;
+
+  .header {
+    width: 70%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    p {
+      font-size: 2rem;
+      justify-content: right;
+      font-weight: 700;
+    }
+  }
+
+  .inputField {
+    display: flex;
+    flex-direction: column;
+    padding: 10px 0px;
+    justify-content: left;
+
+    p {
+      font-size: 1.25rem;
+      font-weight: 700;
+      text-align: left;
+      padding-bottom: 5px;
+    }
+
+    .menuPicture {
+      display: none;
+    }
+
+    .pictureWrapper {
+      display: flex;
+      flex-direction: column;
+
+      justify-content: left;
+      height: 3rem;
+    }
+
+    label {
+      width: 100%;
+      height: 3rem;
+      padding: 0.5rem;
+      padding-right: 1rem;
+      border-radius: var(--radius-m);
+      border: 1px solid var(--gray-100);
+      background-color: var(--gray-100);
+
+      &:focus-visible {
+        border: none;
+        outline: 2px solid var(--main-color);
+      }
+    }
+  }
+`;

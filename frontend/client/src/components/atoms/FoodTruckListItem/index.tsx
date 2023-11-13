4c8df8e98dd1a7ab0a7ae3fe15a627f4f6dc21f1
박은규 <parkeg1223@gmail.com>
2023-11-14 00:54:26 +0900

    fix: 푸드트럭 이미지 사이즈 관련 코드 주석 처리

--- frontend/client/src/components/atoms/FoodTruckListItem/index.tsx
+++ frontend/client/src/components/atoms/FoodTruckListItem/index.tsx
@@ -47,9 +47,9 @@ function FoodTruckListItem(props: IFoodTruckMenuItemProps) {
         <span>({grade})</span>
         <span>{reviewCount}</span>
       </div>
-      <div className="foodTruckImage">
+      {/* <div className="foodTruckImage">
         <img src={foodTruckImageUrl} alt="" />
-      </div>
+      </div> */}
     </FoodTruckListItemWrapper>
   );
 }

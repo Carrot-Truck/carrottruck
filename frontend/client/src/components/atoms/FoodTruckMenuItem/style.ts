a90b8ae15b506b0636812813307fb1b9843a7d23
gunhoo <gunhoo2016@gmail.com>
2023-11-14 14:43:09 +0900

    fix: 사용자 푸드트럭 메뉴 css 수정
    With: S02P31C211-161-review-service-logic

--- frontend/client/src/components/atoms/FoodTruckMenuItem/style.ts
+++ frontend/client/src/components/atoms/FoodTruckMenuItem/style.ts
@@ -3,39 +3,50 @@ import styled from 'styled-components';
 export const FoodTruckMenuItemWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
   width: 100%;
   margin: 0.5rem;
   text-align: center;
   border-top: 1px solid var(--gray-400);
-
+  border: 1px solid #ccc;
+  margin-left: 5px;
   img {
     width: 30%;
+    height: 3rem;
+    padding-right: 5px;
+    padding-left: 5px;
+  }
+
+  .deleteIcon {
+    width: 8%;
     height: 3rem;
   }
 `;
 
+
 export const MenuTextWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  display: flex:
+  flex-direction:column:
   width: 100%;
   justify-content: flex-start;
   text-align: left;
+  margin-left: 5px;
 
-  p:nth-child(1) {
+  p: nth-child(1) {
     font-size: 1.25rem;
-    color: var(--black-300);  // 'font-color' 대신 'color' 사용
+    font-color: var(--black-300);
     font-weight: 900;
     width: 100%;
   }
-  p:nth-child(2) {
+  p: nth-child(2) {
     font-size: 1rem;
-    color: var(--black-300);  // 여기도 동일하게 수정
+    font-color: var(--black-300);
     font-weight: 700;
     width: 100%;
   }
-  p:nth-child(3) {
+  p: nth-child(3) {
     font-size: 0.75rem;
-    color: var(--black-300);  // 여기도 동일하게 수정
+    font-color: var(--black-300);
     font-weight: 400;
     width: 100%;
   }
--- frontend/client/src/components/organisms/FoodTruckMenu/style.ts
+++ frontend/client/src/components/organisms/FoodTruckMenu/style.ts
@@ -3,5 +3,24 @@ import styled from 'styled-components';
 export const FoodTruckMenuContainer = styled.div`
   position: relative;
   width: 100%;
-  border-bottom: 1px solid var(--gray-400);
+  // border-bottom: 1px solid var(--gray-400);
+    background-color: #fff; // 사진에 기반한 배경색
+  // border: 1px solid #ccc; // 경계선 스타일
+  // padding: 16px; // 내부 여백
+  // margin-bottom: 16px; // 하단 여백
+  // margin-right: 2px;
+  font-family: '나눔고딕', 'Nanum Gothic', sans-serif; // 폰트 스타일
+
+  h3 {
+    font-size: 1.2rem; // 제목 폰트 크기
+    color: #000; // 제목 폰트 색상
+    margin-bottom: 8px; // 제목과 내용 사이의 여백
+  }
+
+  p {
+    font-size: 1rem; // 내용 폰트 크기
+    color: #333; // 내용 폰트 색상
+    line-height: 1.5; // 라인 높이
+    margin-bottom: 4px; // 내용 간 여백
+  }
 `;

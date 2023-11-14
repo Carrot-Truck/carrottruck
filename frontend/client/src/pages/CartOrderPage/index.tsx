cc0b124ea956aa0930e2d041c6baa20594bc3064
qhfhdls12 <qhfhdls12@naver.com>
2023-11-14 21:30:27 +0900

    feat: CartOrderPage 수정
    - 주문하기 페이지 일부 수정

--- frontend/client/src/pages/CartOrderPage/index.tsx
+++ frontend/client/src/pages/CartOrderPage/index.tsx
@@ -38,98 +38,98 @@ function CartOrderPage() {
     fetchData();
 
     const loadScript = (src: string, callback: () => void) => {
-        const script = document.createElement('script');
-        script.src = src;
-        script.onload = callback;
-        script.onerror = () => {
-          console.error(`스크립트 로드 실패: ${src}`);
-        };
-        document.head.appendChild(script);
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = callback;
+      script.onerror = () => {
+        console.error(`스크립트 로드 실패: ${src}`);
       };
-
-      loadScript("https://code.jquery.com/jquery-1.12.4.min.js", () => {
-        loadScript("https://cdn.iamport.kr/js/iamport.payment-1.1.7.js", () => {
-        });
-      });
-
-      return () => {
+      document.head.appendChild(script);
     };
 
+    loadScript("https://code.jquery.com/jquery-1.12.4.min.js", () => {
+      loadScript(
+        "https://cdn.iamport.kr/js/iamport.payment-1.1.7.js",
+        () => {}
+      );
+    });
+
+    return () => {};
   }, []);
-  
+
   const loadIamportScript = () => {
     return new Promise<void>((resolve, reject) => {
-        const script = document.createElement('script');
-        script.src = "https://service.iamport.kr/js/iamport.payment-1.1.5.js"; // 아임포트 스크립트 URL
-        script.onload = () => resolve();
-        script.onerror = () => reject(new Error('아임포트 스크립트 로드 실패'));
-        document.head.appendChild(script);
+      const script = document.createElement("script");
+      script.src = "https://service.iamport.kr/js/iamport.payment-1.1.5.js"; // 아임포트 스크립트 URL
+      script.onload = () => resolve();
+      script.onerror = () => reject(new Error("아임포트 스크립트 로드 실패"));
+      document.head.appendChild(script);
     });
-};
+  };
 
-const requestPay = async () => {
-  await loadIamportScript();
+  const requestPay = async () => {
+    await loadIamportScript();
 
-  const IMP = (window as any).IMP;
-  IMP.init(`${process.env.REACT_APP_PAYMENT_CODE}`);
+    const IMP = (window as any).IMP;
+    IMP.init(`${process.env.REACT_APP_PAYMENT_CODE}`);
 
-  IMP.request_pay(
-    {
-      pg: "kakaopay.TC0ONETIME",
-      pay_method: "card",
-      merchant_uid: new Date().getTime(),
-      name: "당근트럭_포장주문",
-      amount: cartOrder?.totalPrice
-    },
-    async (rsp: { imp_uid: string; paid_amount: any }) => {
-      try {
-        const { data } = await axios.post(
-          "http://localhost:8001/api/verifyIamport/" + rsp.imp_uid
-        );
-        if (rsp.paid_amount === data.data.response.amount) {
-          alert("결제 성공");
-          createOrder(
-            (response: AxiosResponse) => {
-              const data = getData(response);
-              console.log("결제 완료 데이터: ", data);
-              navigate("/cart");
-              // 주문내역으로 이동하기
-            },
-            (error: any) => {
-              console.log("결제시도 오류: ", error);
-              navigate("/cart");
-            }
+    IMP.request_pay(
+      {
+        pg: "kakaopay.TC0ONETIME",
+        pay_method: "card",
+        merchant_uid: new Date().getTime(),
+        name: "당근트럭_포장주문",
+        amount: cartOrder?.totalPrice,
+      },
+      async (rsp: { imp_uid: string; paid_amount: any }) => {
+        try {
+          const { data } = await axios.post(
+            "http://localhost:8001/api/verifyIamport/" + rsp.imp_uid
           );
-        } else {
+          if (rsp.paid_amount === data.data.response.amount) {
+            alert("결제 성공");
+            createOrder(
+              (response: AxiosResponse) => {
+                const data = getData(response);
+                console.log("결제 완료 데이터: ", data);
+                navigate("/cart");
+                // 주문내역으로 이동하기
+              },
+              (error: any) => {
+                console.log("결제시도 오류: ", error);
+                navigate("/cart");
+              }
+            );
+          } else {
+            alert("결제 실패");
+            navigate("/cart");
+          }
+        } catch (error) {
+          console.error("Error while verifying payment:", error);
           alert("결제 실패");
-          navigate("/cart");
         }
-      } catch (error) {
-        console.error("Error while verifying payment:", error);
-        alert("결제 실패");
       }
-    }
-  );
-};
+    );
+  };
 
   return (
     <CartOrderPageLayout>
+      <BackSpace></BackSpace>
       <div className="header">
-        <BackSpace></BackSpace>
         <p>주문하기</p>
       </div>
       <div>
-        <p>{cartOrder?.foodTruckName}</p>
-        <p>{cartOrder?.prepareTime}</p>
-        <p>{cartOrder?.phoneNumber}</p>
-        <p>{cartOrder?.totalPrice}</p>
+        <p className="foodTruckName">{cartOrder?.foodTruckName}</p>
+        <p className="prepareTime">{cartOrder?.prepareTime}</p>
+        <p className="phoneNumber">{cartOrder?.phoneNumber}</p>
+        <p className="totalPrice">{cartOrder?.totalPrice}</p>
       </div>
       <Button
         handleClick={requestPay}
         color="Primary"
         size="full"
         radius="m"
-        text= "결제하기"
+        text="결제하기"
       />
       <Navbar />
     </CartOrderPageLayout>
--- frontend/client/src/pages/CartOrderPage/style.ts
+++ frontend/client/src/pages/CartOrderPage/style.ts
@@ -1,52 +1,20 @@
 import { styled } from "styled-components";
 
 export const CartOrderPageLayout = styled.div`
-  width: 360px; /* Set the width of the container */
-  padding-bottom: 200px; /* Add padding to the bottom */
-  color: black; /* Set text color */
-  margin: auto; /* Center the layout in the middle of the page */
-  background-color: #ffffff; /* Set background color to white */
+width: 360px;
+padding-bottom: 200px;
+color: black;
 
-  .header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 16px;
-    background-color: #f7f7f7; /* Light grey background for the header */
-    border-bottom: 1px solid #eaeaea; /* Add a border to the bottom of the header */
-
-    p {
-      font-weight: bold;
-      font-size: 1.5rem; /* Larger text for the page title */
-    }
+.header {
+  p {
+    font-family: BM JUA_TTF;
+    font-size: 2rem;
+    justify-content: center;
   }
+}
 
-  /* Style for each section */
-  div {
-    padding: 16px;
-    border-bottom: 1px solid #eaeaea; /* Add a border to each section */
-
-    p {
-      margin: 0;
-      padding: 8px 0; /* Add padding to top and bottom of text */
-      font-size: 1rem;
-    }
-  }
-
-  /* Style for the button */
-  Button {
-    width: calc(100% - 32px); /* Full width minus padding */
-    margin: 16px;
-    background-color: #ff6b6b; /* Orange background for the button */
-    color: white;
-    border: none;
-    padding: 12px 0; /* Padding inside the button */
-    border-radius: 8px; /* Rounded corners for the button */
-    font-weight: bold;
-    cursor: pointer; /* Pointer cursor on hover */
-
-    &:hover {
-      background-color: #e65a5a; /* Darker orange on hover */
-    }
-  }
+p {
+  font-family: BM JUA_TTF;
+  font-size: 3rem;
+}
 `;

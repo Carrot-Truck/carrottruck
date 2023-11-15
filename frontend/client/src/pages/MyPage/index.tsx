5785232fb6983c771de4a6c952968a1558fe107d
gunhoo <gunhoo2016@gmail.com>
2023-11-15 22:48:25 +0900

    feat: my page에 사용자 정보 갱신

--- frontend/client/src/pages/MyPage/index.tsx
+++ frontend/client/src/pages/MyPage/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { MyPageLayout , LogoutP} from './style';
 import BackHome from 'components/atoms/BackHome';
 import Navbar from 'components/organisms/Navbar';
@@ -8,17 +8,27 @@ import Heart from 'assets/icons/filled_heart.svg';
 import MyReviewForm from 'components/organisms/MyReviewsForm';
 import OrderListForm from 'components/organisms/OrderListForm';
 import { useNavigate } from 'react-router-dom';
+import { info } from 'api/member/client';
+import { AxiosResponse } from 'axios';
+// import FoodTruckList from 'components/organisms/FoodTruckList';
 
+interface User {
+  name : string,
+  nickname : string,
+  email : string,
+  phoneNumber : string,
+  role : string
+}
 function MyPage() {
   const navigate = useNavigate();
   const [selectedButton, setSelectedButton] = useState<number>(1);
-  const userData = {
-    name: '김동현',
-    nickname: '매미킴',
-    email: 'ssafy@gmail.com',
-    phoneNumber: '010-1234-1234',
-    role: 'CLIENT'
-  };
+  const [userData, setUserData] = useState<User>({
+    name: '',
+    nickname: '',
+    email: '',
+    phoneNumber: '',
+    role: ''
+  });
 
   // 클릭 핸들러 함수
   const handleClick = (buttonId: number) => {
@@ -33,6 +43,12 @@ function MyPage() {
     localStorage.clear();
     navigate('/login');
   };
+
+  useEffect(()=>{
+    // TODO: 여기에 내가 좋아요 한 foodTruckList 받아서
+    // 3 번 FoodTruckList 에 뿌려주기
+    info((response: AxiosResponse)=>setUserData(response.data.data), handleLogout)
+  }, [])
 
   return (
     <MyPageLayout>
@@ -60,6 +76,7 @@ function MyPage() {
       </div>
       {selectedButton === 1 && <OrderListForm></OrderListForm>}
       {selectedButton === 2 && <MyReviewForm></MyReviewForm>}
+      {/* {selectedButton === 3 && <FoodTruckList></FoodTruckList>} */}
       <Navbar></Navbar>
       
     </MyPageLayout>

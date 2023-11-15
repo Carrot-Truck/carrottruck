bb8e75acff32209dc6d4b944c2b375305fb3af8f
gunhoo <gunhoo2016@gmail.com>
2023-11-15 21:38:15 +0900

    feat: my review API 연동 및 뷰 css 수정

--- frontend/client/src/components/organisms/MyReviewsForm/index.tsx
+++ frontend/client/src/components/organisms/MyReviewsForm/index.tsx
@@ -1,39 +1,32 @@
+import { useEffect, useState } from 'react';
 import { MyReviewsFormContainer } from './style';
 import MyReview from 'components/atoms/MyReview';
+import { getMyReview } from 'api/review';
+import { useNavigate } from 'react-router-dom';
+import { AxiosResponse } from 'axios';
 
 function MyReviewForm() {
-  const myReviewsList = [
-    {
-      reviewId: 1,
-      reviewFoodTruckDto: {
-        foodTruckName: '내가만든푸드트럭',
-        foodTruckId: 1
-      },
-      grade: 5,
-      createdDate: [2023, 11, 13, 9, 57, 52, 956459500],
-      content: '여기 맛집이다!!',
-      imageUrl:
-        'https://carrottruck.s3.ap-northeast-2.amazonaws.com/review/7e46bb52-07de-4077-b382-feee759e89a7%EC%A7%B1%EA%B5%AC.jpg'
-    },
-    {
-      reviewId: 2,
-      reviewFoodTruckDto: {
-        foodTruckName: '김동현 된장삼겹',
-        foodTruckId: 2
-      },
-      grade: 1,
-      createdDate: [2023, 11, 10, 9, 57, 52, 956459500],
-      content: '여기 진짜 별로에요. 서비스도 최악이고 다신 안올거에요.',
-      imageUrl:
-        'https://carrottruck.s3.ap-northeast-2.amazonaws.com/%EB%8B%B9%EA%B7%BC%ED%8A%B8%EB%9F%AD%EB%A1%9C%EA%B3%A0.png'
-    }
-  ];
+  const navigate = useNavigate();
+  const [myReviewsList, setMyReviewsList] = useState([]);
+
+  const handleSuccess = (response : AxiosResponse) => {
+    setMyReviewsList(response.data.data.myReviewDtoList);
+  }
+
+  const handleFail = () => {
+    alert("리뷰 조회 중 문제가 발생했습니다.\n다시 시도해주세요.");
+    navigate('/');
+  }
+  useEffect(()=>{
+    getMyReview(handleSuccess, handleFail);
+  }, [])
+
   return (
     <MyReviewsFormContainer>
       <div className="title">
         <p>리뷰내역</p>
       </div>
-      {myReviewsList.map((myreview) => (
+      {myReviewsList.map((myreview:any) => (
         <MyReview
           key={myreview.reviewId}
           foodTruckName={myreview.reviewFoodTruckDto.foodTruckName}

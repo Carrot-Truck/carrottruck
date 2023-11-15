6ba66a22e72a6e05cdf7cb3d59478396a3b28097
parkeg1223 <parkeg1223@gmail.com>
2023-11-16 01:24:18 +0900

    feat: client 로그인 페이지 enter 키 이벤트 처리 로직 적용

--- frontend/client/src/components/organisms/LoginForm/index.tsx
+++ frontend/client/src/components/organisms/LoginForm/index.tsx
@@ -14,6 +14,12 @@ function LoginForm() {
   const grantType = localStorage.getItem('grantType');
   const APPLICATION_SPRING_SERVER_URL =
     process.env.NODE_ENV === 'production' ? 'https://k9c211.p.ssafy.io/api' : 'http://localhost:8001/api';
+  
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter') {
+        login();
+      }
+    };
 
   useEffect(() => {
     const isValidUser = async () => {
@@ -62,8 +68,20 @@ function LoginForm() {
     <LoginFormContainer>
       {/* <DownIcon /> */}
       <FieldSet>
-        <Input type="text" value={email} setValue={setEmail} placeholder="이메일 아이디" />
-        <Input type="password" value={password} setValue={setPassword} placeholder="비밀번호" />
+        <Input
+          type="text"
+          value={email}
+          setValue={setEmail}
+          placeholder="이메일 아이디"
+          onKeyPress={handleKeyPress} // 엔터 키 이벤트 추가
+        />
+        <Input
+          type="password"
+          value={password}
+          setValue={setPassword}
+          placeholder="비밀번호"
+          onKeyPress={handleKeyPress} // 엔터 키 이벤트 추가
+        />
       </FieldSet>
       <Button text="로그인" color="Primary" size="full" radius="s" handleClick={login} />
       {showErrorMessage && <p style={{ color: 'red' }}>아이디/패스워드를 확인하세요</p>}

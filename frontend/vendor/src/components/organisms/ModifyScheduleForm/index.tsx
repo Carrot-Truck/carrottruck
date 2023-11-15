ecbfd4828139d99fd0f72aadf4ecc5663e117439
gunhoo <gunhoo2016@gmail.com>
2023-11-15 11:21:18 +0900

    feat: 스케쥴 변경 시간 적용
    With: S02P31C211-161-review-service-logic

--- frontend/vendor/src/components/organisms/ModifyScheduleForm/index.tsx
+++ frontend/vendor/src/components/organisms/ModifyScheduleForm/index.tsx
@@ -7,6 +7,16 @@ import Navbar from '../Navbar';
 import ScheduleMap from "components/atoms/ScheduleMap"
 import { reverseGeocoding } from 'api/address'
 import { AxiosResponse } from 'axios';
+import { createSchedule } from 'api/schedule';
+
+interface ScheduleFormInterface {
+  foodTruckId: number,
+  dayOfWeek: string,
+  startTime: string,
+  endTime: string,
+  latitude: string,
+  longitude: string,
+}
 
 function ModifyScheduleForm() {
   // const location = useLocation();
@@ -17,7 +27,7 @@ function ModifyScheduleForm() {
   const [latitude, setLatitude] = useState('');
   const [startTime, setStartTime] = useState(location.state.startTime);
   const [endTime, setEndTime] = useState(location.state.endTime);
-  // const day = useState(location.state.day);
+  const [day] = useState(location.state.day);
 
   const CLIENT_KEY: string = process.env.REACT_APP_CLIENT_ID || 'your-default-key-or-handle-error';
 
@@ -35,10 +45,39 @@ function ModifyScheduleForm() {
   //   navigate(-1);
   // };
 
+  const validateTimeFormat = (time: string) => {
+    // 정규표현식: 'HH:MM' 형식 검증 (00:00 ~ 23:59)
+    const regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    return regex.test(time);
+  };
+
+  const validateTimeInput = (times : ScheduleFormInterface) =>{
+    if(times.startTime !== '' || times.endTime !== '' || times.latitude !== '' || times.longitude !== ''){
+      if(validateTimeFormat(times.startTime) && validateTimeFormat(times.endTime) && times.latitude !== '' && times.longitude !== ''){
+        return 1; // 입력이 있었고, 두 형식이 모두 유효한 format 이라면 1 반환
+      }
+      return -1; // 입력이 있었지만 두 형식 중 하나라도 유효하지 않다면 다시 입력해야하므로 -1
+    }else{
+      return 0; // 아무 입력이 없는 요일이므로 0 반환
+    }
+  }
+
   const modifyScheduleDetail = () => {
     // 로직을 추가하여 스케줄 등록 추가
-    
-    navigate(-1);
+    const inputRequest = {
+      foodTruckId: Number.parseInt(localStorage.getItem("selectedFoodTruckId") || "0"),
+      dayOfWeek: day, // useState에서 반환된 상태 값만 사용
+      startTime: startTime,
+      endTime: endTime,
+      latitude: latitude,
+      longitude: longitude
+    };
+  
+    if (validateTimeInput(inputRequest) <= 0) {
+      alert("입력이 올바르지 않습니다.");
+      return;
+    }
+    createSchedule(inputRequest, ()=>{ navigate(-1); return;}, ()=>{alert('등록 중 오류 발생!\n관리자에게 문의하세요'); navigate('/');});
   };
 
   return (

2e11e315bbbd63a011f40c9971268e6f46686767
gunhoo <gunhoo2016@gmail.com>
2023-11-16 11:01:55 +0900

    feat: 찜 푸트드럭 내역 목록 출력

--- frontend/client/src/api/foodtruck/foodTruck.tsx
+++ frontend/client/src/api/foodtruck/foodTruck.tsx
@@ -65,3 +65,8 @@ export async function isOpenFoodTruck(foodTruckId: number, success: any, fail: a
   api.defaults.headers["Authorization"] = getAuthorization();
   await api.get(`/open/${foodTruckId}`).then(success).catch(fail);
 }
+
+export async function getLikedFoodTruck( success: any, fail: any) {
+  api.defaults.headers["Authorization"] = getAuthorization();
+  await api.get(`/like`).then(success).catch(fail);
+}

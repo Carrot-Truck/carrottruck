d060e916ce2df00d1058889e9acb11c403100168
gunhoo <gunhoo2016@gmail.com>
2023-11-16 11:59:15 +0900

    fix: 장바구니 css 수정

--- frontend/client/src/components/atoms/CartMenuItem/style.ts
+++ frontend/client/src/components/atoms/CartMenuItem/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
-
 export const CartMenuItemWrapper = styled.div`
   display: flex;
+  align-items: center; /* 이미지와 텍스트를 수직 중앙에 정렬 */
   flex-direction: row;
   width: 100%;
   margin: 0.5rem;
@@ -11,32 +11,59 @@ export const CartMenuItemWrapper = styled.div`
   img {
     width: 30%;
     height: 3rem;
+    object-fit: cover; /* 이미지 비율 유지 */
+  }
+
+  button {
+    background-color: var(--primary-color); /* 버튼 배경색 */
+    color: red; /* 버튼 텍스트 색상 */
+    // padding: 0.5rem 1rem; /* 버튼 패딩 */
+    margin: 1rem;
+    // border: 2px solid red; /* 회색 테두리 추가 */
+    border-radius: 0.5rem; /* 둥근 모서리 */
+    cursor: pointer; /* 마우스 오버시 커서 변경 */
+    font-size: 1rem; /* 폰트 크기 */
   }
 `;
 
 export const MenuTextWrapper = styled.div`
-  display: flex:
-  flex-direction:column:
-  width: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 70%; /* 이미지 영역을 제외한 나머지 너비 */
   justify-content: flex-start;
   text-align: left;
 
-  p: nth-child(1) {
+  /* 버튼을 포함하는 div 스타일링 */
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-around; /* 버튼 간격 조정 */
+  }
+
+  button {
+    background-color: var(--primary-color); /* 버튼 배경색 */
+    color: black; /* 버튼 텍스트 색상 */
+    // padding: 0.5rem 1rem; /* 버튼 패딩 */
+    margin: 1rem;
+    border: 2px solid lightgray; /* 회색 테두리 추가 */
+    border-radius: 0.5rem; /* 둥근 모서리 */
+    cursor: pointer; /* 마우스 오버시 커서 변경 */
+    font-size: 1rem; /* 폰트 크기 */
+  }
+
+  p:nth-child(1) {
     font-size: 1.25rem;
-    font-color: var(--black-300);
+    color: var(--black-300); /* font-color가 아닌 color 사용 */
     font-weight: 900;
-    width: 100%;
   }
-  p: nth-child(2) {
+  p:nth-child(2) {
     font-size: 1rem;
-    font-color: var(--black-300);
+    color: var(--black-300);
     font-weight: 700;
-    width: 100%;
   }
-  p: nth-child(3) {
-    font-size: 0.75rem;
-    font-color: var(--black-300);
+  p:nth-child(3) {
+    font-size: 1rem;
+    color: var(--black-300);
     font-weight: 400;
-    width: 100%;
   }
 `;

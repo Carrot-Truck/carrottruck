42527c934caa11a5795b915aa8cb374354046d77
최영환 <longbright0804@gmail.com>
2023-11-17 09:55:12 +0900

    build: TimeZone Asia/Seoul 설정
    Closes #S09P31C211-281

--- Backend/src/main/java/com/boyworld/carrot/CarrotApplication.java
+++ Backend/src/main/java/com/boyworld/carrot/CarrotApplication.java
@@ -1,10 +1,18 @@
 package com.boyworld.carrot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CarrotApplication {
+
+	@PostConstruct
+	void setDefaultTimezone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarrotApplication.class, args);
